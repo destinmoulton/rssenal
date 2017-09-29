9de0612e3852ce4da7da48a9c35e80f9866887ff
Destin Moulton <destin.moulton@gmail.com>
2017-09-29 13:48:29 -0600

    Added Feeds mongoose controller.

---
+++ src/server/api/controllers/Feeds.controller.js
@@ -0,0 +1,38 @@
+import Feeds from "../models/Feeds.model";
+
+class FeedsController {
+    get_all(req, res){
+        Feeds.find({}, (err, feeds)=>{
+            if(err) 
+                res.send(err);
+            res.json(feeds);
+        });
+    }
+
+    add(req, res){
+        let newFeed = new Feeds(req.body);
+        newFeed.save((err, feed)=>{
+            if(err)
+                res.send(err);
+            res.json(feed);
+        });
+    }
+
+    get_single(req, res){
+        Feeds.findById(req.params.feedId, (err, feed)=>{
+            if(err)
+                res.send(err);
+            res.json(feed);
+        });
+    }
+
+    delete_single(req, res){
+        Feeds.remove({_id: req.params.feedId}, (err)=>{
+            if(err)
+                res.send(err);
+            res.json({message: "Feed deleted."});
+        })
+    }
+}
+
+export default new FeedsController();
