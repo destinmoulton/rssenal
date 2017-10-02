bf701f08bb83fb3a992b6297583aa7cc5b203ed7
Destin Moulton <destin.moulton@gmail.com>
2017-10-02 10:30:21 -0600

    Added Feed Groups routes.

--- dist/server/api.routes.js
+++ dist/server/api.routes.js
@@ -5,9 +5,14 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 exports.default = function (app) {
+    app.use('/api/feedgroups', _FeedGroups2.default);
     app.use('/api/feeds', _Feeds2.default);
     app.use('/api/validatefeedurl', _ValidateFeedURL2.default);
 };
+
+var _FeedGroups = require("./api/routes/FeedGroups.route");
+
+var _FeedGroups2 = _interopRequireDefault(_FeedGroups);
 
 var _Feeds = require("./api/routes/Feeds.route");
 
