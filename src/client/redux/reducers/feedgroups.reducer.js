dd4d352f533b740ab7479abf40a4baa81c8bc807
Destin Moulton <destin.moulton@gmail.com>
2017-10-02 11:57:09 -0600

    Added a FETCHING case.

--- src/client/redux/reducers/feedgroups.reducer.js
+++ src/client/redux/reducers/feedgroups.reducer.js
@@ -1,12 +1,36 @@
-import { List } from "immutable";
+import { OrderedMap } from "immutable";
 
+import { 
+    FEEDGROUPS_FETCHING,
+    FEEDGROUPS_RECEIVED
+} from "../actiontypes";
 
 const INITIAL_STATE = {
-    groups: List()
+    groups: OrderedMap(),
+    isFetchingFeedGroups: false,
+    hasFeedGroups: false
 };
 
 const FeedGroupsReducer = function(state = INITIAL_STATE, action){
     switch(action.type){
+        case FEEDGROUPS_FETCHING:
+            return {
+                ...state,
+                isFetchingFeedGroups: true
+            }
+        case FEEDGROUPS_RECEIVED:
+            let keyedGroups = [];
+            action.groups.map((group)=>{
+                keyedGroups.push({[group._id]: group});
+            });
+
+            const freshGroups = OrderedMap(keyedGroups);
+
+            return {
+                ...state,
+                groups: freshGroups,
+                hasFeedGroups: true
+            }
         default:
             return state;
     }
