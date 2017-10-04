e3828a5f8abb4c11414c57764959478f135db2a6
Destin Moulton <destin.moulton@gmail.com>
2017-10-04 15:10:59 -0600

    Moved ListFeedGroups.

--- src/client/Components/ListFeedGroups/ListFeedGroups.js
+++ src/client/Components/ListFeedGroups/ListFeedGroups.js
@@ -2,9 +2,9 @@ import React, {Component} from "react";
 
 import { connect } from "react-redux";
 
-import { getAllFeedGroups } from "../redux/actions/feedgroups.actions";
+import { getAllFeedGroups } from "../../redux/actions/feedgroups.actions";
 
-import GroupItem from "./ListFeedGroups/GroupItem";
+import GroupItem from "./ListGroupItem";
 
 class ListFeedGroups extends Component {
 
