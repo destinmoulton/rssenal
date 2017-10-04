73f88020892bfa743a02d532172eb61cb7e4385b
Destin Moulton <destin.moulton@gmail.com>
2017-10-04 16:58:01 -0600

    Added feed validation url constant.

--- src/client/redux/apiendpoints.js
+++ src/client/redux/apiendpoints.js
@@ -1,4 +1,6 @@
 export const API_BASE = "/api";
 
+export const API_FEEDVALIDATION_BASE = API_BASE + "/validatefeedurl/";
+
 export const API_FEEDGROUPS_BASE = API_BASE + "/feedgroups/";
 export const API_FEEDGROUPS_GET_ALL = API_FEEDGROUPS_BASE;
