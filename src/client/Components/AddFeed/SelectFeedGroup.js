be83e6accc2cc7f16cb97387917ae2b967b5bc66
Destin Moulton <destin.moulton@gmail.com>
2017-10-05 11:43:41 -0600

    Created a Select Feed Group component.

---
+++ src/client/Components/AddFeed/SelectFeedGroup.js
@@ -0,0 +1,70 @@
+import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+
+import { Dropdown } from "semantic-ui-react";
+
+class SelectFeedGroup extends Component {
+    static propTypes = {
+        onChange: PropTypes.func.isRequired
+    }
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selectedValue: "0"
+        }
+
+        this._handleOnChange = this._handleOnChange.bind(this);
+    }
+
+    _handleOnChange(e, dropdown){
+        
+        const selectedValue = dropdown.value;
+        this.setState({
+            selectedValue
+        });
+
+        this.props.onChange(selectedValue);
+    }
+
+    render() {
+        const { feedgroups } = this.props;
+        const { selectedValue } = this.state;
+        
+        // Convert the feed groups Immutable list into
+        // an array of key/value options.
+        let groupOptions = feedgroups.map((group)=>{
+            return {
+                key: group._id,
+                value: group._id,
+                text: group.name
+            };
+        }).toArray();
+
+        groupOptions.unshift({key: "0", value: "0", text:"Uncategorized"});
+        
+        return (
+            <Dropdown 
+                value={selectedValue}
+                options={groupOptions}
+                onChange={this._handleOnChange}
+                selection
+            />
+        );
+    }
+}
+const mapStateToProps = (state)=>{
+    const { feedgroups } = state;
+    return {
+        feedgroups: feedgroups.groups
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(SelectFeedGroup);
