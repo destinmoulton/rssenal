0a414e2952c2905e09d27d7b27e90a2713585142
Destin Moulton <destin.moulton@gmail.com>
2017-10-05 14:32:27 -0600

    Added URL API endpoint for the feeds.

--- src/client/redux/apiendpoints.js
+++ src/client/redux/apiendpoints.js
@@ -4,3 +4,5 @@ export const API_FEEDVALIDATION_BASE = API_BASE + "/validatefeedurl/";
 
 export const API_FEEDGROUPS_BASE = API_BASE + "/feedgroups/";
 export const API_FEEDGROUPS_GET_ALL = API_FEEDGROUPS_BASE;
+
+export const API_FEEDS_BASE = API_BASE + "/feeds/";
