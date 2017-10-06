53c0870b23c59c57ef351f39d73fa6ea813b56fd
Destin Moulton <destin.moulton@gmail.com>
2017-10-06 11:05:42 -0600

    Added entries actions.

---
+++ src/client/redux/actions/entries.actions.js
@@ -0,0 +1,83 @@
+
+import {
+    ENTRIES_GET_BEGIN,
+    ENTRIES_GET_COMPLETE,
+    ENTRIES_UPDATE_BEGIN,
+    ENTRIES_UPDATE_COMPLETE
+} from "../actiontypes";
+
+import {
+    API_ENTRIES_BASE
+} from "../apiendpoints";
+
+export function beginUpdateAndGetEntries(){
+    return (dispatch)=>{
+        dispatch(beginUpdateProcess());
+        dispatch(updateEntries());
+    };
+}
+
+function beginUpdateProcess(){
+    return {
+        type: ENTRIES_UPDATE_BEGIN
+    }
+}
+
+function updateEntries(){
+    return (dispatch)=>{
+        const url = API_ENTRIES_BASE;
+        const init = {
+            method: "PUT",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }            
+        };
+
+        fetch(url, init)
+            .then((res)=>{
+                return res.json();
+            })
+            .then((resObj)=>{
+                dispatch(beginGetEntries());
+            })
+    }
+}
+
+export function beginGetEntries(){
+    return (dispatch)=>{
+        dispatch(beginGetProcess());
+        dispatch(getEntries());
+    }
+}
+
+function beginGetProcess(){
+    return {
+        type: ENTRIES_GET_BEGIN
+    }
+}
+
+function getEntries(){
+    return (dispatch)=>{
+        const url = API_ENTRIES_BASE;
+        const init = {
+            method: "GET"
+        };
+
+        fetch(url, init)
+            .then((res)=>{
+                return res.json();
+            })
+            .then((resObj)=>{
+                dispatch(getEntriesComplete(resObj.entries));
+            })
+    }
+}
+
+function getEntriesComplete(entries){
+    return {
+        type: ENTRIES_GET_COMPLETE,
+        entries
+    }
+}
+
