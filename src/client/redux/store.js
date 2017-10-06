991e598a704f96283308f40df96ff20fdb7cb183
Destin Moulton <destin.moulton@gmail.com>
2017-10-06 11:06:57 -0600

    Added entries reducer.

--- src/client/redux/store.js
+++ src/client/redux/store.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import { routerReducer } from "react-router-redux"
 import thunk from "redux-thunk";
 
+import entriesReducer from "./reducers/entries.reducer";
 import feedsReducer from "./reducers/feeds.reducer";
 import feedGroupsReducer from "./reducers/feedgroups.reducer";
 
 const store = createStore(
   combineReducers({
+    entries: entriesReducer,
     feedgroups: feedGroupsReducer,
     feeds: feedsReducer
   }),
