3d45fc610018be7433cd864e58a65e8240c94a9f
Destin Moulton <destin.moulton@gmail.com>
2017-10-09 13:47:02 -0600

    Added filterReducer.

--- src/client/redux/store.js
+++ src/client/redux/store.js
@@ -3,12 +3,14 @@ import { routerReducer } from "react-router-redux"
 import thunk from "redux-thunk";
 
 import entriesReducer from "./reducers/entries.reducer";
+import filterReducer from "./reducers/filter.reducer";
 import feedsReducer from "./reducers/feeds.reducer";
 import feedGroupsReducer from "./reducers/feedgroups.reducer";
 
 const store = createStore(
   combineReducers({
     entries: entriesReducer,
+    filter: filterReducer,
     feedgroups: feedGroupsReducer,
     feeds: feedsReducer
   }),
