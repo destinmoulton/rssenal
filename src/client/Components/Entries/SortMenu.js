472235b8e0078ea959a022494bb3b411c4041b9e
Destin Moulton <destin.moulton@gmail.com>
2017-10-10 16:11:17 -0600

    Added sort menu component.

---
+++ src/client/Components/Entries/SortMenu.js
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+
+import { Select } from "semantic-ui-react";
+
+const MENU_OPTIONS = [
+    {key: "publish_date:asc", value: "publish_date:asc", text: "Date - Oldest First"},
+    {key: "publish_date:desc", value: "publish_date:desc", text: "Date - Newest First"},
+    {key: "title:asc", value: "title:asc", text: "Title"},
+];
+
+class SortMenu extends Component {
+    static propTypes = {
+        currentSortBy: PropTypes.string.isRequired,
+        onChange: PropTypes.func.isRequired
+    };
+
+    render() {
+        const { currentSortBy, onChange } = this.props;
+        return (
+            <Select
+                options={MENU_OPTIONS}
+                onChange={onChange}
+                defaultValue={currentSortBy}/>
+        );
+    }
+}
+
+export default SortMenu;
