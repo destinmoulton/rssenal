69ff5fb58f98871c63d87a4139206a7bb928da77
Destin Moulton <destin.moulton@gmail.com>
2017-10-13 11:34:26 -0600

    Moved the Buttons and Group/Feed modal logic into the child ListFeedGroups component.

--- src/client/Components/RSSEnal.js
+++ src/client/Components/RSSEnal.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Menu, Segment } from "semantic-ui-react";
 
-import AddFeedModal from "./AddFeed/AddFeedModal";
-import AddGroup from "./ListFeedGroups/AddGroup";
 import EntriesList from "./Entries/EntriesList";
 import ListFeedGroups from "./ListFeedGroups/ListFeedGroups";
 
@@ -12,10 +10,12 @@ class RSSEnal extends Component {
         super(props);
 
         this.state = {
-            entriesFilter: {}
+            entriesFilter: {},
+            
         };
 
         this._handleChangeEntriesFilter = this._handleChangeEntriesFilter.bind(this);
+        
     }
 
     _handleChangeEntriesFilter(newFilter){
@@ -25,18 +25,16 @@ class RSSEnal extends Component {
     }
 
     render(){
-        const { entriesFilter } = this.state;
+        const { 
+            entriesFilter
+        } = this.state;
 
         return (
             <div>
                 <div className="rss-body-left-container">
-                    <div>
-                        <AddFeedModal />
-                    </div>
-                    <div>
-                        <ListFeedGroups onFilterChange={this._handleChangeEntriesFilter}/>
-                        <AddGroup />
-                    </div>
+                    <ListFeedGroups
+                        onFilterChange={this._handleChangeEntriesFilter}
+                    />
                 </div>
                 <div className="rss-body-right-container">
                     <EntriesList filter={entriesFilter} />
