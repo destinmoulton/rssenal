4a40c3be3ebd2b5b65fbd3285614c7a3532d5ae1
Destin Moulton <destin.moulton@gmail.com>
2017-10-13 11:36:27 -0600

    Moved buttons and modals from rrsenal in here.

--- src/client/Components/ListFeedGroups/ListFeedGroups.js
+++ src/client/Components/ListFeedGroups/ListFeedGroups.js
@@ -1,19 +1,27 @@
-import React, {Component} from "react";
 
+import React, {Component} from "react";
 import { connect } from "react-redux";
 
 import { beginUpdateAndGetEntries } from "../../redux/actions/entries.actions";
 import { getAllFeedGroups } from "../../redux/actions/feedgroups.actions";
 import { getAllFeeds } from "../../redux/actions/feeds.actions";
 
-
+import AddFeedModal from "../Modals/AddFeedModal";
+import ButtonBar from "./ButtonBar";
+import GroupEditorModal from "../Modals/GroupEditorModal";
 import GroupItem from "./GroupItem";
 
-
 class ListFeedGroups extends Component {
-
     constructor(props){
         super(props);
+
+        this.state = {
+            editGroup: {},
+            editGroupModalOpen: false
+        }
+
+        this._handleCloseEditGroupModal = this._handleCloseEditGroupModal.bind(this);
+        this._handleEditGroup = this._handleEditGroup.bind(this);
     }
 
     componentWillMount(){
@@ -31,21 +39,49 @@ class ListFeedGroups extends Component {
         }
     }
 
+    _handleEditGroup(group){
+        this.setState({
+            editGroup: group,
+            editGroupModalOpen: true
+        })
+    }
+
+    _handleCloseEditGroupModal(){
+        this.setState({
+            editGroup: {},
+            editGroupModalOpen: false
+        });
+    }
+
     render(){
+        const { 
+            editGroup,
+            editGroupModalOpen,
+        } = this.state;
+
         const { groups } = this.props;
 
         let listFeedGroups = [];
         groups.map((group)=>{
             const groupBlock = <div key={group._id} >
-                                   <GroupItem group={group} />
-                                   
+                                   <GroupItem group={group} editGroup={this._handleEditGroup}/>
                                </div>;
             listFeedGroups.push(groupBlock);
         })
         
         return(
             <div>
-                {listFeedGroups}
+                <div>
+                    <AddFeedModal />
+                    <ButtonBar openEditGroupModal={this._handleEditGroup}/>
+                    <GroupEditorModal 
+                        isModalOpen={editGroupModalOpen}
+                        onCloseModal={this._handleCloseEditGroupModal}
+                        group={editGroup}/>
+                </div>
+                <div>
+                    {listFeedGroups}
+                </div>
             </div>
         );
     }
