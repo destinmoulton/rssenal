c2350f115fe49f8275cd8c30662654e074fe18c9
Destin Moulton <destin.moulton@gmail.com>
2017-10-20 15:38:53 -0600

    Built decrement unread reduction.

--- src/client/redux/reducers/feeds.reducer.js
+++ src/client/redux/reducers/feeds.reducer.js
@@ -3,6 +3,7 @@ import { Map, OrderedMap } from "immutable";
 import {
     FEEDS_ADD_BEGIN,
     FEEDS_ADD_COMPLETE,
+    FEEDS_DECREMENT_UNREAD,
     FEEDS_GETALL_COMPLETE,
     FEEDS_SETALL_UNREAD_COUNT,
     FEEDS_UPDATE_BEGIN,
@@ -36,6 +37,30 @@ function feedsReducer(state = INITIAL_STATE, action){
                 ...state,
                 feeds: newFeeds,
                 isAddingFeed: false
+            }
+        }
+        case FEEDS_DECREMENT_UNREAD:{
+            const { unreadMap, feeds } = state;
+            const { entry } = action;
+            const { feed } = feeds.get(entry.feed_id);
+
+            const unreadFeeds = unreadMap.feeds;
+            const unreadGroups = unreadMap.groups;
+
+            if(unreadFeeds.has(entry.feed_id)){
+                const unreadCount = unreadFeeds.get(entry.feed_id);
+                unreadFeeds = unreadFeeds.set(entry.feed_id, unreadCount - 1);
+            }
+            
+            if(unreadGroups.has(feed.group_id)){
+                const unreadCount = unreadGroups.get(feed.group_id);
+                unreadGroups = unreadGroups.set(feed.group_id, unreadCount - 1);
+            }
+
+            const unreadMap = {"feeds": unreadFeeds, "groups": unreadGroups};
+            return {
+                ...state,
+                unreadMap
             }
         }
         case FEEDS_GETALL_COMPLETE:{
