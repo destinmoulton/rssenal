fb94d537428de0b4f8a8432bcc9b13a655ffeceb
Destin Moulton <destin.moulton@gmail.com>
2017-10-26 13:48:12 -0600

    Renamed all js files to ts or tsx (if react).

--- src/client/lib/sort.ts
+++ src/client/lib/sort.ts
@@ -5,7 +5,7 @@
  * @param b object the second element to compare
  * @param propertyName string The object property to compare
  */
-export function compareAscByProp(a, b, propertyName){
+export function compareAscByProp(a: Object, b: Object, propertyName: string){
     if(a[propertyName] < b[propertyName]){ return -1; }
     if(a[propertyName] > b[propertyName]){ return 1; }
     if(a[propertyName] === b[propertyName]){ return 0;}
