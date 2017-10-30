9f28aa27f8887fe02907d99beb9fb07dc04b8b84
Destin Moulton <destin.moulton@gmail.com>
2017-10-30 13:39:29 -0600

    Added a GetState interface for redux.

--- src/client/interfaces.ts
+++ src/client/interfaces.ts
@@ -1,11 +1,16 @@
 import { Map, OrderedMap } from "immutable";
 
+/** Redux Interfaces */
 export interface IDispatch {
     <R>(asyncAction: (dispatch: IDispatch, getState: () => IRootStoreState) => R): R;
     <R>(asyncAction: (dispatch: IDispatch) => R): R;
     // (neverAction: (dispatch: Dispatch, getState: () => GetState) => never): never;
     (action: object): void;
     // (action: Thunk): ; // thunks in this app must return a promise
+}
+
+export interface IGetState {
+    ():IRootStoreState;
 }
 
 export type TEntryID = string;
