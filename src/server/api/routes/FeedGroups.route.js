a00c9543ab81740812fca5cc67823d70501cb027
Destin Moulton <destin.moulton@gmail.com>
2017-11-01 11:58:56 -0600

    Added route to update multiple feed groups.

--- src/server/api/routes/FeedGroups.route.js
+++ src/server/api/routes/FeedGroups.route.js
@@ -5,8 +5,10 @@ import feedGroups from "../controllers/FeedGroups.controller";
 
 router.get('/', feedGroups.get_all);
 router.post('/', feedGroups.add);
+router.put('/', feedGroups.update_multiple);
 
 router.get('/:feedGroupId', feedGroups.get_single);
 router.delete('/:feedGroupId', feedGroups.delete_single);
 router.put('/:feedGroupId', feedGroups.update_single);
+
 export default router;
