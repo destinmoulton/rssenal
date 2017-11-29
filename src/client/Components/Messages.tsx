b1f886004805c266f9057a6b75f26bf4b1edac0b
Destin Moulton <destin.moulton@gmail.com>
2017-11-29 15:38:19 -0700

    Added code to add and remove messages in the notification system.

--- src/client/Components/Messages.tsx
+++ src/client/Components/Messages.tsx
@@ -1,26 +1,61 @@
+import { is } from "immutable";
 import * as React from "react";
+import * as NotificationSystem from "react-notification-system";
 import { connect } from "react-redux";
 
-import { IDispatch, IRootStoreState, TMessages } from "../interfaces";
+import { removeMessage } from "../redux/actions/messages.actions";
+
+import { IDispatch, IMessage, IReducerStateMessages, IRootStoreState, TMessages } from "../interfaces";
 
 interface IMapStateToProps {
     messages: TMessages
 }
 
 interface IMapDispatchToProps {
-
+    removeMessage: (message: IMessage)=>IReducerStateMessages
 }
 
 interface IMessagesProps extends IMapStateToProps, IMapDispatchToProps {
 
 }
 
+interface IRefs {
+    notificationSystem: NotificationSystem.System
+}
+
 class Messages extends React.Component<IMessagesProps> {
+    private _refs: IRefs = {
+        notificationSystem: null
+    };
+
+    constructor(props: IMessagesProps){
+        super(props);
+
+        this._onCloseMessage = this._onCloseMessage.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps: IMessagesProps){
+        const newMessages = nextProps.messages;
+        const oldMessages = this.props.messages;
+
+        newMessages.map((newMessage: IMessage)=>{
+            if(!oldMessages.includes(newMessage)){
+                newMessage.onRemove = this._onCloseMessage;
+                newMessage.position = "br";
+                this._refs.notificationSystem.addNotification(newMessage);
+            }
+        });
+    }
+
+    _onCloseMessage(message: IMessage){
+        this.props.removeMessage(message);
+    }
+
     render(){
         return (
-            <div>
-                
-            </div>
+            <NotificationSystem 
+                ref={(ref: NotificationSystem.System)=>this._refs.notificationSystem = ref}
+            />
         );
     }
 }
@@ -33,7 +68,7 @@ const mapStateToProps = (state: IRootStoreState): IMapStateToProps =>{
 
 const mapDispatchToProps = (dispatch: IDispatch)=>{
     return {
-
+        removeMessage: (message: IMessage)=>dispatch(removeMessage(message))
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Messages);
