4f7dbb7346234ce4aa50193bc15430cdf03af07d
Destin Moulton <destin.moulton@gmail.com>
2017-11-29 15:38:39 -0700

    Added messages actions with message() and removeMessage() methods.

---
+++ src/client/redux/actions/messages.actions.ts
@@ -0,0 +1,19 @@
+import { MESSAGES_ADD, MESSAGES_REMOVE } from "../actiontypes";
+import { IMessage } from "../../interfaces";
+
+export function message(messageText: string, level: string){
+    return {
+        type: MESSAGES_ADD,
+        message: {
+            message: messageText,
+            level
+        }
+    }
+}
+
+export function removeMessage(message: IMessage){
+    return {
+        type: MESSAGES_REMOVE,
+        message
+    }
+}
