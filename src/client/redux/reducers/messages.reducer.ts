7ee57cf4a11d384ab497ebcf09547a1bf4854bdc
Destin Moulton <destin.moulton@gmail.com>
2017-11-29 15:37:34 -0700

    Added logic to add a message and added a remove reduction.

--- src/client/redux/reducers/messages.reducer.ts
+++ src/client/redux/reducers/messages.reducer.ts
@@ -1,18 +1,32 @@
 import { List } from "immutable";
 
-import { MESSAGES_ADD } from "../actiontypes";
+import { MESSAGES_ADD, MESSAGES_REMOVE } from "../actiontypes";
 
 import { IMessage, IMessageAction,IReducerStateMessages } from "../../interfaces";
 
 const INITIAL_STATE: IReducerStateMessages = {
-    messages: List()
+    messages: List(),
+    lastUID: 0
 }
 
 const messagesReducer = function(state = INITIAL_STATE, action: IMessageAction){
     switch(action.type){
         case MESSAGES_ADD:
+            const nextUID = state.lastUID + 1;
+            const newMessage = (Object as any).assign({}, action.message);
+            newMessage.uid = nextUID;
             return {
-                ...state
+                ...state,
+                messages: state.messages.push(newMessage)
+            }
+        case MESSAGES_REMOVE:
+            const index = state.messages.findIndex((message: IMessage)=>{
+                return message.uid === action.message.uid;
+            });
+            
+            return {
+                ...state,
+                messages: state.messages.remove(index)
             }
         default:
             return state;
