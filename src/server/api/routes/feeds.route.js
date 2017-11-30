08d864ef2bc07ebfa10c1605fe6b7f4e37d058c5
Destin Moulton <destin.moulton@gmail.com>
2017-11-30 16:11:54 -0700

    Moved feeds route.

--- src/server/api/routes/feeds.route.js
+++ src/server/api/routes/feeds.route.js
@@ -9,4 +9,4 @@ router.post('/', feeds.add);
 router.get('/:feedId', feeds.get_single);
 router.delete('/:feedId', feeds.delete_single);
 router.put('/:feedId', feeds.update_single);
-export default router;
+module.exports = router;
