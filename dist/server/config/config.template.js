f26ff2fd5a86f2987774be53c3e2de3da4fbbeef
Destin Moulton <destin.moulton@gmail.com>
2018-01-02 15:20:31 -0700

    Added a config template to configure the server values.

---
+++ dist/server/config/config.template.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = config = {
+    port: 3000,
+    mongo: {
+        uri: "mongodb://localhost/rssenal"
+    },
+    auth: {
+        user: "usernamehere",
+        password: "newpasswordhere"
+    }
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9zZXJ2ZXIvY29uZmlnL2NvbmZpZy50ZW1wbGF0ZS5qcyJdLCJuYW1lcyI6WyJjb25maWciLCJwb3J0IiwibW9uZ28iLCJ1cmkiLCJhdXRoIiwidXNlciIsInBhc3N3b3JkIl0sIm1hcHBpbmdzIjoiOzs7OztrQkFBZ0JBLFNBQVM7QUFDckJDLFVBQU0sSUFEZTtBQUVyQkMsV0FBTztBQUNIQyxhQUFLO0FBREYsS0FGYztBQUtyQkMsVUFBTTtBQUNGQyxjQUFNLGNBREo7QUFFRkMsa0JBQVU7QUFGUjtBQUxlLEMiLCJmaWxlIjoiY29uZmlnLnRlbXBsYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgKGNvbmZpZyA9IHtcbiAgICBwb3J0OiAzMDAwLFxuICAgIG1vbmdvOiB7XG4gICAgICAgIHVyaTogXCJtb25nb2RiOi8vbG9jYWxob3N0L3Jzc2VuYWxcIlxuICAgIH0sXG4gICAgYXV0aDoge1xuICAgICAgICB1c2VyOiBcInVzZXJuYW1laGVyZVwiLFxuICAgICAgICBwYXNzd29yZDogXCJuZXdwYXNzd29yZGhlcmVcIlxuICAgIH1cbn0pO1xuIl19
---
+++ src/server/config/config.template.js
@@ -0,0 +1,10 @@
+export default (config = {
+    port: 3000,
+    mongo: {
+        uri: "mongodb://localhost/rssenal"
+    },
+    auth: {
+        user: "usernamehere",
+        password: "newpasswordhere"
+    }
+});
