45a5d1da5f3f31c938c50e9a26a2f7a9438914fe
Destin Moulton <destin.moulton@gmail.com>
2018-01-03 15:53:17 -0700

    Added reducers for the auth actions.

--- src/client/redux/reducers/auth.reducer.ts
+++ src/client/redux/reducers/auth.reducer.ts
@@ -1,12 +1,36 @@
+import {
+    AUTH_ERROR,
+    AUTH_USER_IS_AUTHENTIC,
+    AUTH_LOGOUT
+} from "../actiontypes";
 import { IAuthAction, IReducerStateAuth } from "../../interfaces";
 
 const INITIAL_STATE: IReducerStateAuth = {
+    authenticationError: "",
     isAuthorized: false,
     jwtSecret: ""
 };
 
 const authReducer = (state = INITIAL_STATE, action: IAuthAction) => {
     switch (action.type) {
+        case AUTH_ERROR:
+            return {
+                ...state,
+                authenticationError: action.message
+            };
+        case AUTH_LOGOUT:
+            return {
+                ...state,
+                isAuthorized: false,
+                jwtSecret: ""
+            };
+        case AUTH_USER_IS_AUTHENTIC:
+            return {
+                ...state,
+                authenticationError: "",
+                isAuthorized: true,
+                jwtSecret: action.username + action.password
+            };
         default:
             return { ...state };
     }
