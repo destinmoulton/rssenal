7aa824aba1fcb69570ae6e535d8e3f419347b105
Destin Moulton <destin.moulton@gmail.com>
2018-01-03 11:43:41 -0700

    Added form.

--- src/client/Components/Login.tsx
+++ src/client/Components/Login.tsx
@@ -1,8 +1,65 @@
 import * as React from "react";
-
+import {
+    Button,
+    Form,
+    Grid,
+    Header,
+    Image,
+    Message,
+    Segment
+} from "semantic-ui-react";
 import { IDispatch } from "../interfaces";
 
-class Login extends React.Component {}
+interface IMapDispatchToProps {
+    authorizeUser: () => {};
+}
+interface ILogin extends IMapDispatchToProps {}
+class Login extends React.Component<ILogin> {
+    render() {
+        return (
+            <div className="login-form">
+                <style>{`
+                body > div,
+                body > div > div,
+                body > div > div > div.login-form {
+                    height: 100%;
+                }
+                `}</style>
+                <Grid
+                    textAlign="center"
+                    style={{ height: "100%" }}
+                    verticalAlign="middle"
+                >
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <Header as="h2" color="teal" textAlign="center">
+                            RSSenal Sign In
+                        </Header>
+                        <Form size="large">
+                            <Segment stacked>
+                                <Form.Input
+                                    fluid
+                                    icon="user"
+                                    iconPosition="left"
+                                    placeholder="Username"
+                                />
+                                <Form.Input
+                                    fluid
+                                    icon="lock"
+                                    iconPosition="left"
+                                    placeholder="Password"
+                                    type="password"
+                                />
+                                <Button color="teal" fluid size="large">
+                                    Login
+                                </Button>
+                            </Segment>
+                        </Form>
+                    </Grid.Column>
+                </Grid>
+            </div>
+        );
+    }
+}
 
 const mapDispatchToProps = (dispatch: IDispatch) => {};
 export default Login;
