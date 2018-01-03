2b9ba8d1ca84604d4db4a72ac2c12e5e97b304e9
Destin Moulton <destin.moulton@gmail.com>
2018-01-03 15:50:47 -0700

    Created logout button component.

---
+++ src/client/Components/Entries/LogoutButton.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { connect } from "react-redux";
+import { Button } from "semantic-ui-react";
+
+import { logoutUser } from "../../redux/actions/auth.actions";
+import { IRootStoreState, IDispatch } from "../../interfaces";
+interface IMapStateToProps {}
+interface IMapDispatchToProps {
+    logoutUser: () => void;
+}
+interface ILogoutProps extends IMapStateToProps, IMapDispatchToProps {}
+class LogoutButton extends React.Component<ILogoutProps> {
+    constructor(props: ILogoutProps) {
+        super(props);
+
+        this._handleClickLogout = this._handleClickLogout.bind(this);
+    }
+
+    _handleClickLogout() {
+        this.props.logoutUser();
+    }
+
+    render() {
+        return (
+            <Button
+                icon="sign out"
+                onClick={this._handleClickLogout}
+                size="tiny"
+            />
+        );
+    }
+}
+
+const mapStateToProps = (state: IRootStoreState) => {
+    return {};
+};
+
+const mapDispatchToProps = (dispatch: IDispatch) => {
+    return {
+        logoutUser: () => dispatch(logoutUser())
+    };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(LogoutButton);
