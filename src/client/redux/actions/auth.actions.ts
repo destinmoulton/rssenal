c768bcff2aaf8931c0086ecfb4fedfe07038fafc
Destin Moulton <destin.moulton@gmail.com>
2018-01-03 15:04:04 -0700

    Added returned function.

--- src/client/redux/actions/auth.actions.ts
+++ src/client/redux/actions/auth.actions.ts
@@ -1,21 +1,25 @@
 import { API_AUTH } from "../apiendpoints";
 import { JSON_HEADERS } from "../../lib/headers";
 
-export const authenticateUser = (username, password) => {
-    const url = API_AUTH;
-    const init = {
-        method: "POST",
-        body: JSON.stringify({ username, password }),
-        headers: JSON_HEADERS
-    };
+import { IDispatch } from "../../interfaces";
 
-    fetch(url, init)
-        .then(res => {
-            return res.json();
-        })
-        .then(auth => {
-            if (auth.isAuthorized) {
-            }
-        })
-        .catch(err => {});
-};
+export function authenticateUser(username: string, password: string) {
+    return (dispatch: IDispatch) => {
+        const url = API_AUTH;
+        const init = {
+            method: "POST",
+            body: JSON.stringify({ username, password }),
+            headers: JSON_HEADERS
+        };
+
+        fetch(url, init)
+            .then(res => {
+                return res.json();
+            })
+            .then(auth => {
+                if (auth.isAuthorized) {
+                }
+            })
+            .catch(err => {});
+    };
+}
