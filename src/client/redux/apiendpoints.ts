51d1492b1931666743a356d77c34a2b22f2910ba
Destin Moulton <destin.moulton@gmail.com>
2018-01-03 10:54:16 -0700

    Added auth endpoint; reordered for alphabetical.

--- src/client/redux/apiendpoints.ts
+++ src/client/redux/apiendpoints.ts
@@ -1,10 +1,12 @@
 export const API_BASE = "/api";
 
+export const API_AUTH = "/auth/";
+
 export const API_ENTRIES_BASE = API_BASE + "/entries/";
 
 export const API_FEEDVALIDATION_BASE = API_BASE + "/validatefeedurl/";
 
+export const API_FEEDS_BASE = API_BASE + "/feeds/";
+
 export const API_FOLDERS_BASE = API_BASE + "/folders/";
 export const API_FOLDERS_GET_ALL = API_FOLDERS_BASE;
-
-export const API_FEEDS_BASE = API_BASE + "/feeds/";
