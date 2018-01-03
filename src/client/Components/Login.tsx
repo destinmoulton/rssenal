44bdcea1621651359441e6470c4fe79afaf02fab
Destin Moulton <destin.moulton@gmail.com>
2018-01-03 11:01:21 -0700

    Added basic Login component.

---
+++ src/client/Components/Login.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+import { IDispatch } from "../interfaces";
+
+class Login extends React.Component {}
+
+const mapDispatchToProps = (dispatch: IDispatch) => {};
+export default Login;
