a0141dca83016a6fcee0e1ae29e655136f919165
Destin Moulton <destin.moulton@gmail.com>
2018-01-03 15:04:19 -0700

    Added type for the action.

--- src/client/redux/reducers/auth.reducer.ts
+++ src/client/redux/reducers/auth.reducer.ts
@@ -1,11 +1,11 @@
-import { IReducerStateAuth } from "../../interfaces";
+import { IAuthAction, IReducerStateAuth } from "../../interfaces";
 
 const INITIAL_STATE: IReducerStateAuth = {
     isAuthorized: false,
     jwtSecret: ""
 };
 
-const authReducer = (state = INITIAL_STATE, action) => {
+const authReducer = (state = INITIAL_STATE, action: IAuthAction) => {
     switch (action.type) {
         default:
             return { ...state };
