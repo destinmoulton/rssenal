92665ffb57ff6e5ffb581cc41f3ccc478844d0b6
Destin Moulton <destin.moulton@gmail.com>
2018-01-03 15:53:26 -0700

    Added actions for the auth mechanism.

--- src/client/redux/actions/auth.actions.ts
+++ src/client/redux/actions/auth.actions.ts
@@ -1,6 +1,12 @@
 import { API_AUTH } from "../apiendpoints";
 import { JSON_HEADERS } from "../../lib/headers";
 
+import {
+    AUTH_ERROR,
+    AUTH_LOGOUT,
+    AUTH_USER_IS_AUTHENTIC
+} from "../actiontypes";
+
 import { IDispatch } from "../../interfaces";
 
 export function authenticateUser(username: string, password: string) {
@@ -17,9 +23,37 @@ export function authenticateUser(username: string, password: string) {
                 return res.json();
             })
             .then(auth => {
-                if (auth.isAuthorized) {
+                if (auth.status === "success") {
+                    dispatch(userAuthenticated(username, password));
+                } else {
+                    dispatch(
+                        authenticationError(
+                            "Unable to find that username or password."
+                        )
+                    );
                 }
             })
             .catch(err => {});
+    };
+}
+
+function userAuthenticated(username: string, password: string) {
+    return {
+        type: AUTH_USER_IS_AUTHENTIC,
+        username,
+        password
+    };
+}
+
+function authenticationError(message: string) {
+    return {
+        type: AUTH_ERROR,
+        message
+    };
+}
+
+export function logoutUser() {
+    return {
+        type: AUTH_LOGOUT
     };
 }
