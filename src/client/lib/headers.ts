f555cec9a38c99c31062f7a384c11e49eb93d078
Destin Moulton <destin.moulton@gmail.com>
2018-01-04 12:09:05 -0700

    Added methods to generate versions of jwt headers for fetch operations.

--- src/client/lib/headers.ts
+++ src/client/lib/headers.ts
@@ -3,3 +3,26 @@ jsonHeaders.append("Accept", "application/json");
 jsonHeaders.append("Content-Type", "application/json");
 
 export const JSON_HEADERS = jsonHeaders;
+
+// Build the jwt header for json access
+export function generateJWTJSONHeaders() {
+    let headers = new Headers();
+    headers.append("Accept", "application/json");
+    headers.append("Content-Type", "application/json");
+
+    const jwt_token = localStorage.getItem("jwt_token");
+    if (jwt_token) {
+        headers.append("Authorization", `Bearer ${jwt_token}`);
+    }
+    return headers;
+}
+
+//Build the jwt header for general access
+export function generateJWTHeaders() {
+    let headers = new Headers();
+    const jwt_token = localStorage.getItem("jwt_token");
+    if (jwt_token) {
+        headers.append("Authorization", `Bearer ${jwt_token}`);
+    }
+    return headers;
+}
