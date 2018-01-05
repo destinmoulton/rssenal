c10b70785ec22a0217de2d172df23291b683b2ba
Destin Moulton <destin.moulton@gmail.com>
2018-01-05 16:36:09 -0700

    Converted ordered map conversion from tuple to set.

--- src/client/redux/reducers/entries.reducer.ts
+++ src/client/redux/reducers/entries.reducer.ts
@@ -1,52 +1,64 @@
-
 import { OrderedMap } from "immutable";
 
 import {
     ENTRIES_GET_COMPLETE,
     ENTRIES_MARKREAD_COMPLETE,
-    ENTRIES_GET_BEGIN
+    ENTRIES_REMOVE_FEED
 } from "../actiontypes";
 
-import { TEntryID, IEntry, IEntriesAction, IReducerStateEntries } from "../../interfaces";
+import {
+    TEntryID,
+    IEntry,
+    IEntriesAction,
+    IReducerStateEntries
+} from "../../interfaces";
 
 const INITIAL_STATE: IReducerStateEntries = {
-    entries: OrderedMap<TEntryID, IEntry>(),
-    isGettingEntries: false
+    entries: OrderedMap<TEntryID, IEntry>()
 };
 
 type IEntryTuple = [TEntryID, IEntry];
 
-function entriesReducer(state = INITIAL_STATE, action: IEntriesAction){
-    switch(action.type){
-        case ENTRIES_GET_BEGIN: 
-            return {
-                ...state,
-                isGettingEntries: true
-            }
+function entriesReducer(state = INITIAL_STATE, action: IEntriesAction) {
+    switch (action.type) {
         case ENTRIES_GET_COMPLETE: {
             const { entries } = state;
 
-            let tuples: IEntryTuple[] = [];
-            action.entries.forEach((entry)=>{
-                tuples.push([entry._id, entry]);
+            let newEntries = OrderedMap<TEntryID, IEntry>();
+            action.entries.forEach(entry => {
+                newEntries = entries.set(entry._id, entry);
             });
-            
-            return {
-                ...state,
-                entries: OrderedMap(tuples),
-                isGettingEntries: false
-            }
-        }
-        case ENTRIES_MARKREAD_COMPLETE:{
-            const { entries } = state;
-            const newEntries = entries.set(action.newEntry._id, action.newEntry);
+
             return {
                 ...state,
                 entries: newEntries
-            }
+            };
+        }
+        case ENTRIES_REMOVE_FEED:
+            const { entries } = state;
+
+            const newEntries = entries.filter((entry: IEntry) => {
+                return entry.feed_id !== action.feedId;
+            });
+
+            return {
+                ...state,
+                entries: newEntries
+            };
+
+        case ENTRIES_MARKREAD_COMPLETE: {
+            const { entries } = state;
+            const newEntries = entries.set(
+                action.newEntry._id,
+                action.newEntry
+            );
+            return {
+                ...state,
+                entries: newEntries
+            };
         }
         default:
-            return {...state};
+            return { ...state };
     }
 }
 
