a6e3f184584596364e3cec144b8dd0344e38fdac
Destin Moulton <destin.moulton@gmail.com>
2018-01-10 13:39:38 -0700

    Added blockquote styles.

--- src/client/lib/sanitizer.ts
+++ src/client/lib/sanitizer.ts
@@ -10,6 +10,7 @@ export const sanitizeEntryContent = (
     let allowedTags = [
         "a",
         "b",
+        "blockquote",
         "br",
         "code",
         "em",
@@ -34,6 +35,7 @@ export const sanitizeEntryContent = (
 
     let allowedAttributes = {
         a: ["href", "target"],
+        blockquote: <any>[],
         code: <any>[],
         figure: <any>[],
         figcaption: <any>[],
