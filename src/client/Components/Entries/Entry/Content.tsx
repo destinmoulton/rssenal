840dbf156717de63470cc7a0decb12d789488e0e
Destin Moulton <destin.moulton@gmail.com>
2018-01-11 11:59:15 -0700

    Removed time calculation (now in ammended timeAgo field).

--- src/client/Components/Entries/Entry/Content.tsx
+++ src/client/Components/Entries/Entry/Content.tsx
@@ -1,4 +1,3 @@
-import * as moment from "moment";
 import * as React from "react";
 
 import { Icon } from "semantic-ui-react";
@@ -8,23 +7,22 @@ import { sanitizeEntryContent } from "../../../lib/sanitizer";
 import { IEntry } from "../../../interfaces";
 
 interface IContentProps {
-    activeFeedTitle: string;
     entry: IEntry;
     shouldShowImages: boolean;
 }
 
 const Content = (props: IContentProps) => {
-    const { activeFeedTitle, entry, shouldShowImages } = props;
+    const { entry, shouldShowImages } = props;
+
     const creator =
         entry.creator !== undefined || entry.creator !== ""
             ? " by " + entry.creator
             : "";
-    const timeAgo = moment(entry.publish_date).fromNow();
 
     const info = (
         <div className="rss-entry-info-container">
-            {activeFeedTitle}
-            {creator} -- {timeAgo}
+            {entry.feedTitle}
+            {creator} -- {entry.timeAgo}
         </div>
     );
 
