4504fd5113c63f3067dcd6a40dbfa88ab0dc1dfa
Destin Moulton <destin.moulton@gmail.com>
2018-03-28 09:16:47 -0600

    Added login form header style.

--- src/client/Components/Login.tsx
+++ src/client/Components/Login.tsx
@@ -73,7 +73,7 @@ class Login extends React.Component<ILoginProps> {
                     verticalAlign="middle"
                 >
                     <Grid.Column style={{ maxWidth: 450 }}>
-                        <Header as="h2" color="teal" textAlign="center">
+                        <Header as="h2" className="rss-login-header">
                             RSSenal Sign In
                         </Header>
                         <Form size="large">
