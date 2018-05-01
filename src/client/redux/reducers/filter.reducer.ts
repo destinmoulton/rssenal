eca20af1050d97e90bdb3ce7fde02996c0eaa062
Destin Moulton <destin.moulton@gmail.com>
2018-05-01 15:51:54 -0600

    Added FILTER_RESET reduction.

--- src/client/redux/reducers/filter.reducer.ts
+++ src/client/redux/reducers/filter.reducer.ts
@@ -1,26 +1,28 @@
-
-import {
-    FILTER_CHANGE
-} from "../actiontypes"
+import { FILTER_CHANGE, FILTER_RESET } from "../actiontypes";
 
 import { IFilterAction, IReducerStateFilter } from "../../interfaces";
 
 const INITIAL_STATE: IReducerStateFilter = {
-    filter: {limit: "folder", id: "all"}
+    filter: { limit: "folder", id: "all" }
 };
 
-function filterReducer(state = INITIAL_STATE, action: IFilterAction){
-    switch(action.type){
+function filterReducer(state = INITIAL_STATE, action: IFilterAction) {
+    switch (action.type) {
         case FILTER_CHANGE: {
             return {
                 ...state,
                 filter: action.newFilter
-            }
+            };
+        }
+        case FILTER_RESET: {
+            return {
+                ...INITIAL_STATE
+            };
         }
         default:
             return {
                 ...state
-            }
+            };
     }
 }
 
