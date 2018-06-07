7e1acdf018da467822b52d58b006091f6eb9a4cf
Destin Moulton <destin.moulton@gmail.com>
2018-06-07 17:10:42 -0600

    Removed global enzyme.

--- test-client-setup.js
+++ test-client-setup.js
@@ -2,14 +2,6 @@ const enzyme = require("enzyme");
 const Adapter = require("enzyme-adapter-react-16");
 const jestEnzyme = require("jest-enzyme");
 
-//
-// Enzyme Globals
-//
-global.enzyme = enzyme;
-global.shallow = enzyme.shallow;
-global.render = enzyme.render;
-global.mount = enzyme.mount;
-
 enzyme.configure({
     adapter: new Adapter()
 });
