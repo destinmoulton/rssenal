5839629abe446ff8be7f39d04487b803a7da42e0
Destin Moulton <destin.moulton@gmail.com>
2018-06-08 14:19:58 -0600

    Added snapshot test.

--- src/client/components/Entries/__tests__/SortMenu.spec.tsx
+++ src/client/components/Entries/__tests__/SortMenu.spec.tsx
@@ -1,3 +1,18 @@
 import * as React from "react";
 
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
+
+import SortMenu from "../SortMenu";
+
+describe("<SortMenu />", () => {
+    it("matches snapshot", () => {
+        const mockChange = jest.fn();
+        const wrapper = mount(
+            <SortMenu
+                currentSortBy={"publish_date:asc"}
+                onChange={mockChange}
+            />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+});
