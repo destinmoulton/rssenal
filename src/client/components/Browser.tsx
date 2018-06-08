ac7b78eac91693506e7ec76633f917365844bbce
Destin Moulton <destin.moulton@gmail.com>
2018-06-08 15:07:47 -0600

    Moved components and containers into RightPane folder.

--- src/client/components/Browser.tsx
+++ src/client/components/Browser.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import EntriesListContainer from "../containers/Entries/EntriesListContainer";
+import EntriesListContainer from "../containers/RightPane/EntriesListContainer";
 import LeftPaneContainer from "../containers/LeftPane/LeftPaneContainer";
 import MessagesContainer from "../containers/MessagesContainer";
 
