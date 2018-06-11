751ff4e7321c10465ff4095e8700750c791540fa
Destin Moulton <destin.moulton@gmail.com>
2018-06-11 16:45:44 -0600

    Added code to generate the RightPane.

--- src/client/components/RightPane/RightPaneComponent.tsx
+++ src/client/components/RightPane/RightPaneComponent.tsx
@@ -1,1 +1,47 @@
 import * as React from "react";
+
+import TopBarComponent from "../RightPane/TopBarComponent";
+import EntriesListContainer from "../../containers/RightPane/EntriesListContainer";
+
+import * as Types from "../../interfaces";
+
+export interface IRightPaneMapState {
+    filterTitle: string;
+}
+
+export interface IRightPaneMapDispatch {}
+
+type TAll = IRightPaneMapDispatch & IRightPaneMapState;
+
+interface IState {
+    sortBy: string;
+}
+
+class RightPaneComponent extends React.Component<TAll, IState> {
+    state = {
+        sortBy: "publish_date:asc"
+    };
+
+    _handleChangeSort = (e: any, data: any) => {
+        this.setState({
+            sortBy: data.value
+        });
+    };
+
+    render() {
+        const { filterTitle } = this.props;
+        const { sortBy } = this.state;
+        return (
+            <div>
+                <TopBarComponent
+                    title={filterTitle}
+                    onChangeSort={this._handleChangeSort}
+                    sortBy={sortBy}
+                />
+                <EntriesListContainer sortBy={sortBy} />
+            </div>
+        );
+    }
+}
+
+export default RightPaneComponent;
