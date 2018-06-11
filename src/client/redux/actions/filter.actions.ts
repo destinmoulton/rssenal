78794d56428675cdb853b5c20234c2ce428341ea
Destin Moulton <destin.moulton@gmail.com>
2018-06-11 16:46:25 -0600

    Added filter switch to the changeFilter method.

--- src/client/redux/actions/filter.actions.ts
+++ src/client/redux/actions/filter.actions.ts
@@ -1,17 +1,35 @@
 import { FILTER_CHANGE, FILTER_RESET } from "../actiontypes";
 
-import { IDispatch, IFilter } from "../../interfaces";
+import * as Types from "../../interfaces";
 
-export function changeFilter(newFilter: IFilter) {
-    return (dispatch: IDispatch) => {
-        dispatch(setNewFilter(newFilter));
+export function changeFilter(filter: Types.IFilter) {
+    return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
+        const { feeds, folders } = getState();
+        let title = "All";
+
+        switch (filter.limit) {
+            case "feed":
+                const activeFeed = feeds.feeds.get(filter.id);
+                title = activeFeed.title;
+                break;
+            case "folder":
+                if (filter.id !== "all") {
+                    const activeFolder = folders.folders.get(filter.id);
+
+                    title = activeFolder.name;
+                }
+                break;
+        }
+
+        dispatch(setNewFilter(filter, title));
     };
 }
 
-function setNewFilter(newFilter: IFilter) {
+function setNewFilter(newFilter: Types.IFilter, title: string) {
     return {
         type: FILTER_CHANGE,
-        newFilter
+        newFilter,
+        filterTitle: title
     };
 }
 
