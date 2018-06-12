727791d86516db830337cfa6dd89a6af09924892
Destin Moulton <destin.moulton@gmail.com>
2018-06-12 13:55:14 -0600

    Added a DEFAULT_SETTINGS tuple.

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -3,3 +3,25 @@ export const SORT_MENU_OPTIONS = [
     { value: "publish_date:desc", text: "Date - Newest First" },
     { value: "title:asc", text: "Title" }
 ];
+
+// Tuples of default settings
+export const DEFAULT_SETTINGS = [
+    [
+        "show_images",
+        {
+            name: "Show Images",
+            type: "toggle",
+            refresh_entries_on_change: false,
+            value: true
+        }
+    ],
+    [
+        "show_entries_has_read",
+        {
+            name: "Show Entries Already Read",
+            type: "toggle",
+            refresh_entries_on_change: true,
+            value: false
+        }
+    ]
+];
