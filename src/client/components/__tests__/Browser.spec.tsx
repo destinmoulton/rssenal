6635433f60a6d1487df625f3902bbedc97265c35
Destin Moulton <destin.moulton@gmail.com>
2018-06-18 16:37:50 -0600

    Removed global headers type definition.

--- src/client/components/__tests__/Browser.spec.tsx
+++ src/client/components/__tests__/Browser.spec.tsx
@@ -1,17 +1,6 @@
 import * as React from "react";
 import { shallow } from "enzyme";
 
-declare global {
-    namespace NodeJS {
-        interface Global {
-            Headers: () => void;
-        }
-    }
-}
-
-global.Headers = jest.fn().mockImplementation(() => {
-    return { append: () => {} };
-});
 import Browser from "../Browser";
 
 describe("<Browser />", () => {
