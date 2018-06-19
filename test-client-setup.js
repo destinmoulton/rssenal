5419e038b8781e55c37108c04b0e1349efa2461e
Destin Moulton <destin.moulton@gmail.com>
2018-06-19 11:51:54 -0600

    Added global Response, Headers, Request from fetch-ponyfill.

--- test-client-setup.js
+++ test-client-setup.js
@@ -1,7 +1,14 @@
 const enzyme = require("enzyme");
 const Adapter = require("enzyme-adapter-react-16");
 const jestEnzyme = require("jest-enzyme");
+const fetchPonyFill = require("fetch-ponyfill");
+
+const { Request, Response, Headers } = fetchPonyFill();
 
 enzyme.configure({
     adapter: new Adapter()
 });
+
+global.Response = Response;
+global.Headers = Headers;
+global.Request = Request;
