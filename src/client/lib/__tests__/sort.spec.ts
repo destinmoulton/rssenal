aab5f591776acca4978beb225dc37aeff525eb66
Destin Moulton <destin.moulton@gmail.com>
2018-06-19 14:12:19 -0600

    Added test for the string option.

--- src/client/lib/__tests__/sort.spec.ts
+++ src/client/lib/__tests__/sort.spec.ts
@@ -1,13 +1,7 @@
-import {
-    propertyComparator,
-    comparatorAsc,
-    comparatorDesc,
-    stringComparatorAsc,
-    stringComparatorDesc
-} from "../sort";
+import { propertyComparator } from "../sort";
 
-const OBJECT_LESS = { name: "A", number: 1 };
-const OBJECT_MORE = { name: "b", number: 2 }; // b is lower case on purpose to test string comp
+const OBJECT_LESS = { name: "a", number: 1 }; // a is lower case on purpose to test string comp
+const OBJECT_MORE = { name: "B", number: 2 };
 
 describe("lib/sort.ts", () => {
     describe("propertyComparator()", () => {
@@ -83,8 +77,76 @@ describe("lib/sort.ts", () => {
             });
         });
 
-        it("asc string", () => {});
+        describe("asc string", () => {
+            it("returns -1 for a before b", () => {
+                const comp = propertyComparator(
+                    OBJECT_LESS,
+                    OBJECT_MORE,
+                    "asc",
+                    "name",
+                    true
+                );
 
-        it("desc string", () => {});
+                expect(comp).toBe(-1);
+            });
+            it("returns 1 for a after b", () => {
+                const comp = propertyComparator(
+                    OBJECT_MORE,
+                    OBJECT_LESS,
+                    "asc",
+                    "name",
+                    true
+                );
+
+                expect(comp).toBe(1);
+            });
+            it("returns 0 for equivalent", () => {
+                const comp = propertyComparator(
+                    OBJECT_LESS,
+                    OBJECT_LESS,
+                    "asc",
+                    "name",
+                    true
+                );
+
+                expect(comp).toBe(0);
+            });
+        });
+
+        describe("desc string", () => {
+            it("returns -1 when a gt b", () => {
+                const comp = propertyComparator(
+                    OBJECT_MORE,
+                    OBJECT_LESS,
+                    "desc",
+                    "name",
+                    true
+                );
+
+                expect(comp).toBe(-1);
+            });
+            it("returns 1 when a lt b", () => {
+                const comp = propertyComparator(
+                    OBJECT_LESS,
+                    OBJECT_MORE,
+                    "desc",
+                    "name",
+                    true
+                );
+
+                expect(comp).toBe(1);
+            });
+            it("returns 0 for equivalent", () => {
+                const comp = propertyComparator(
+                    OBJECT_LESS,
+                    OBJECT_LESS,
+                    "desc",
+                    "name",
+                    true
+                );
+
+                expect(comp).toBe(0);
+            });
+        });
     });
 });
