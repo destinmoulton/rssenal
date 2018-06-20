fe0ea587c48283fd2494366fe22df5b1d9cb32c3
Destin Moulton <destin.moulton@gmail.com>
2018-06-20 15:19:06 -0600

    Created a ListFeeds spec/test and snapshot.

---
+++ src/client/components/LeftPane/__tests__/ListFeeds.spec.tsx
@@ -0,0 +1,21 @@
+import { Map } from "immutable";
+import * as React from "react";
+
+import { shallow } from "enzyme";
+import * as Types from "../../../types";
+import ListFeeds from "../ListFeeds";
+
+import feeds from "../../../../../test/data/feeds";
+
+describe("<ListFeeds />", () => {
+    it("renders and matches the snapshot", () => {
+        const wrapper = shallow(
+            <ListFeeds
+                feeds={feeds}
+                editFeed={jest.fn()}
+                folderId={"5a54f74838e78863ac6a8961"}
+            />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+});
