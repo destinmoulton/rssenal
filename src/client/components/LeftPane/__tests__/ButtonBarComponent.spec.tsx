f3e263bdaec0f05f9f99d114e093062464c7dac6
Destin Moulton <destin.moulton@gmail.com>
2018-06-20 15:01:06 -0600

    Created tests and snapshots.

---
+++ src/client/components/LeftPane/__tests__/ButtonBarComponent.spec.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+import { shallow } from "enzyme";
+
+import ButtonBarComponent from "../ButtonBarComponent";
+import { Button } from "semantic-ui-react";
+describe("<ButtonBarComponent />", () => {
+    it("renders and matches the snapshot", () => {
+        const wrapper = shallow(
+            <ButtonBarComponent
+                refreshAllFeeds={jest.fn()}
+                openAddFeedModal={jest.fn()}
+                openEditFolderModal={jest.fn()}
+            />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+});
