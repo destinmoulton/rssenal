4a3b9c4104608d91d70e1dccb0b0115168cf8e18
Destin Moulton <destin.moulton@gmail.com>
2018-06-20 11:11:15 -0600

    Removed jest reset mock

--- src/client/lib/__tests__/storage.spec.ts
+++ src/client/lib/__tests__/storage.spec.ts
@@ -3,7 +3,6 @@ import storage from "../storage";
 describe("lib/storage.ts", () => {
     beforeEach(() => {
         localStorage.clear();
-        jest.resetAllMocks();
     });
     it("set() and has() work", () => {
         storage.set("test", "CONTENT");
@@ -11,7 +10,7 @@ describe("lib/storage.ts", () => {
     });
 
     it("get() retrieves a stored item", () => {
-        storage.set("test", "CONTENT");
-        expect(storage.get("test")).toBe("CONTENT");
+        storage.set("testget", "get CONTENT");
+        expect(storage.get("testget")).toBe("get CONTENT");
     });
 });
