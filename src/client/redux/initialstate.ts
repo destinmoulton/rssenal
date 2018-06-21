4637ce6ad2c701563904d1ed91ea6eede9e153d0
Destin Moulton <destin.moulton@gmail.com>
2018-06-21 15:38:03 -0600

    Created ENTRIES_INITIAL_STATE for the entries reducer.

--- src/client/redux/initialstate.ts
+++ src/client/redux/initialstate.ts
@@ -1,7 +1,12 @@
+import { OrderedMap } from "immutable";
 import * as Types from "../types";
 
 export const AUTH_INITIAL_STATE: Types.IReducerStateAuth = {
     authenticationError: "",
     isAuthorized: false,
     isValidatingToken: false
+};
+
+export const ENTRIES_INITIAL_STATE: Types.IReducerStateEntries = {
+    entries: OrderedMap<Types.TEntryID, Types.IEntry>()
 };
