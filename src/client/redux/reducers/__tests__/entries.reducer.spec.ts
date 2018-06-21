9f158a541a71a4018a6ffcb5bb77ca1de3791bd9
Destin Moulton <destin.moulton@gmail.com>
2018-06-21 15:44:47 -0600

    Added test for ENTRIES_SET_ALL reduction.

--- src/client/redux/reducers/__tests__/entries.reducer.spec.ts
+++ src/client/redux/reducers/__tests__/entries.reducer.spec.ts
@@ -3,11 +3,22 @@ import * as ACT_TYPES from "../../actiontypes";
 import entriesReducer from "../entries.reducer";
 
 import { ENTRIES_INITIAL_STATE } from "../../initialstate";
-
+import DATA_ENTRIES from "../../../../../test/data/entries";
 describe("auth reducer", () => {
     it("should return initial state", () => {
         const reduction = entriesReducer(undefined, { type: null });
 
         expect(reduction).toEqual(ENTRIES_INITIAL_STATE);
+    });
+
+    it("should handle ENTRIES_SET_ALL", () => {
+        const action = {
+            type: ACT_TYPES.ENTRIES_SET_ALL,
+            entries: DATA_ENTRIES
+        };
+        const reduction = entriesReducer(undefined, action);
+        expect(reduction).toEqual({
+            entries: DATA_ENTRIES
+        });
     });
 });
