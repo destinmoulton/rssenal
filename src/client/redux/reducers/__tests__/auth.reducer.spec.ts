9f4ce49215627642d1c1bd959c63676daa3d799a
Destin Moulton <destin.moulton@gmail.com>
2018-06-21 15:22:58 -0600

    Added test for AUTH_ERROR action type.

--- src/client/redux/reducers/__tests__/auth.reducer.spec.ts
+++ src/client/redux/reducers/__tests__/auth.reducer.spec.ts
@@ -10,4 +10,22 @@ describe("auth reducer", () => {
 
         expect(reduction).toEqual(AUTH_INITIAL_STATE);
     });
+
+    it("should handle AUTH_ERROR", () => {
+        const message = "Test Auth Error";
+        const action = {
+            type: ACT_TYPES.AUTH_ERROR,
+            message
+        };
+        const reduction = authReducer(undefined, action);
+        expect(reduction).toEqual({
+            authenticationError: message,
+            isAuthorized: false,
+            isValidatingToken: false
+        });
+    });
+
+    it("should handle AUTH_LOGOUT", () => {});
+
+    it("should handle AUTH_USER_IS_AUTHENTIC", () => {});
 });
