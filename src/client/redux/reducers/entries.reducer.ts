1b8d44f9d3d6aca820a09536c9b7bb35ce13392b
Destin Moulton <destin.moulton@gmail.com>
2018-06-21 15:37:47 -0600

    Moved initial state into the initialstate file.

--- src/client/redux/reducers/entries.reducer.ts
+++ src/client/redux/reducers/entries.reducer.ts
@@ -1,19 +1,8 @@
-import { OrderedMap } from "immutable";
-
 import { ENTRIES_SET_ALL } from "../actiontypes";
+import { ENTRIES_INITIAL_STATE } from "../initialstate";
+import { IEntriesAction } from "../../types";
 
-import {
-    TEntryID,
-    IEntry,
-    IEntriesAction,
-    IReducerStateEntries
-} from "../../types";
-
-const INITIAL_STATE: IReducerStateEntries = {
-    entries: OrderedMap<TEntryID, IEntry>()
-};
-
-function entriesReducer(state = INITIAL_STATE, action: IEntriesAction) {
+function entriesReducer(state = ENTRIES_INITIAL_STATE, action: IEntriesAction) {
     switch (action.type) {
         case ENTRIES_SET_ALL: {
             return {
