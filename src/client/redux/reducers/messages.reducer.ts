39551fb01b8ab972e5a5880ab9a9af6f2c81872d
Destin Moulton <destin.moulton@gmail.com>
2018-06-22 11:46:39 -0600

    Moved initial state into initialstate.

--- src/client/redux/reducers/messages.reducer.ts
+++ src/client/redux/reducers/messages.reducer.ts
@@ -1,16 +1,14 @@
-import { List } from "immutable";
-
 import { MESSAGES_ADD, MESSAGES_REMOVE } from "../actiontypes";
 
-import { IMessage, IMessageAction,IReducerStateMessages } from "../../types";
+import { IMessage, IMessageAction, IReducerStateMessages } from "../../types";
 
-const INITIAL_STATE: IReducerStateMessages = {
-    messages: List(),
-    lastUID: 0
-}
+import { MESSAGES_INITIAL_STATE } from "../initialstate";
 
-const messagesReducer = function(state = INITIAL_STATE, action: IMessageAction){
-    switch(action.type){
+const messagesReducer = function(
+    state = MESSAGES_INITIAL_STATE,
+    action: IMessageAction
+) {
+    switch (action.type) {
         case MESSAGES_ADD:
             const nextUID = state.lastUID + 1;
             const newMessage = (Object as any).assign({}, action.message);
@@ -19,19 +17,19 @@ const messagesReducer = function(state = INITIAL_STATE, action: IMessageAction){
                 ...state,
                 messages: state.messages.push(newMessage),
                 lastUID: nextUID
-            }
+            };
         case MESSAGES_REMOVE:
-            const index = state.messages.findIndex((message: IMessage)=>{
+            const index = state.messages.findIndex((message: IMessage) => {
                 return message.uid === action.message.uid;
             });
-            
+
             return {
                 ...state,
                 messages: state.messages.remove(index)
-            }
+            };
         default:
             return state;
     }
-}
+};
 
 export default messagesReducer;
