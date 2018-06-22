ed98003b8b7f9f11c0c46f38e10590e29fda6087
Destin Moulton <destin.moulton@gmail.com>
2018-06-22 14:50:35 -0600

    Refactored the message and messageRemove into completions.

--- src/client/redux/actions/messages.actions.ts
+++ src/client/redux/actions/messages.actions.ts
@@ -1,19 +1,52 @@
-import { MESSAGES_ADD, MESSAGES_REMOVE } from "../actiontypes";
-import { IMessage } from "../../types";
+import { Notification } from "react-notification-system";
 
-export function message(messageText: string, level: string) {
-    return {
-        type: MESSAGES_ADD,
-        message: {
+import {
+    MESSAGES_ADD_COMPLETE,
+    MESSAGES_REMOVE_COMPLETE
+} from "../actiontypes";
+import * as Types from "../../types";
+
+export function message(messageText: string, level: any) {
+    return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
+        const { lastUID, messages } = getState().messagesStore;
+
+        const nextUID: number = lastUID + 1;
+
+        const message: Types.IMessage = {
             message: messageText,
-            level
-        }
+            level,
+            uid: nextUID
+        };
+
+        const newMessages = messages.push(message);
+        dispatch(messageAddComplete(newMessages, nextUID));
     };
 }
 
-export function messageRemove(message: IMessage) {
+function messageAddComplete(messages: Types.TMessages, lastUID: number) {
     return {
-        type: MESSAGES_REMOVE,
-        message
+        type: MESSAGES_ADD_COMPLETE,
+        messages,
+        lastUID
+    };
+}
+
+export function messageRemove(messageToRemove: Types.IMessage) {
+    return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
+        const { messages } = getState().messagesStore;
+
+        const index = messages.findIndex((message: Types.IMessage) => {
+            return message.uid === messageToRemove.uid;
+        });
+
+        const newMessages = messages.remove(index);
+        dispatch(messageRemoveComplete(newMessages));
+    };
+}
+
+function messageRemoveComplete(messages: Types.TMessages) {
+    return {
+        type: MESSAGES_REMOVE_COMPLETE,
+        messages
     };
 }
