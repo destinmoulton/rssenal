b851e0667f8442b786d8d7e84c839b6255c830d2
Destin Moulton <destin.moulton@gmail.com>
2018-06-22 11:40:30 -0600

    Created a spec/test file for the folders reducer.

---
+++ src/client/redux/reducers/__tests__/folders.reducer.spec.ts
@@ -0,0 +1,90 @@
+import { Map, Set } from "immutable";
+
+import * as ACT_TYPES from "../../actiontypes";
+import DATA_ENTRY from "../../../../../test/data/entry";
+import DATA_FEEDS from "../../../../../test/data/feeds";
+import DATA_FEED from "../../../../../test/data/feed";
+import DATA_FOLDERS from "../../../../../test/data/folders";
+import foldersReducer from "../folders.reducer";
+
+import { FOLDERS_INITIAL_STATE } from "../../initialstate";
+import * as Types from "../../../types";
+
+describe("folders reducer", () => {
+    it("should return initial state", () => {
+        const reduction = foldersReducer(undefined, { type: null });
+
+        expect(reduction).toEqual(FOLDERS_INITIAL_STATE);
+    });
+
+    it("should reduce FOLDERS_FETCHING", () => {
+        const action: Types.IFolderAction = {
+            type: ACT_TYPES.FOLDERS_FETCHING
+        };
+        const reduction = foldersReducer(undefined, action);
+        expect(reduction).toEqual({
+            ...FOLDERS_INITIAL_STATE,
+            isFetchingFolders: true
+        });
+    });
+
+    it("should reduce FOLDERS_RECEIVED", () => {
+        const action: Types.IFolderAction = {
+            type: ACT_TYPES.FOLDERS_RECEIVED,
+            folders: DATA_FOLDERS
+        };
+        const reduction = foldersReducer(undefined, action);
+        expect(reduction).toEqual({
+            ...FOLDERS_INITIAL_STATE,
+            isFetchingFolders: false,
+            hasFolders: true,
+            folders: DATA_FOLDERS
+        });
+    });
+
+    it("should reduce FOLDERS_ADD_BEGIN", () => {
+        const action: Types.IFolderAction = {
+            type: ACT_TYPES.FOLDERS_ADD_BEGIN
+        };
+        const reduction = foldersReducer(undefined, action);
+        expect(reduction).toEqual({
+            ...FOLDERS_INITIAL_STATE,
+            isSavingFolder: true
+        });
+    });
+
+    it("should reduce FOLDERS_ADD_COMPLETE", () => {
+        const action: Types.IFolderAction = {
+            type: ACT_TYPES.FOLDERS_ADD_COMPLETE,
+            folders: DATA_FOLDERS
+        };
+        const reduction = foldersReducer(undefined, action);
+        expect(reduction).toEqual({
+            ...FOLDERS_INITIAL_STATE,
+            isSavingFolder: false,
+            folders: DATA_FOLDERS
+        });
+    });
+
+    it("should reduce FOLDERS_DELETE_BEGIN", () => {
+        const action: Types.IFolderAction = {
+            type: ACT_TYPES.FOLDERS_DELETE_BEGIN
+        };
+        const reduction = foldersReducer(undefined, action);
+        expect(reduction).toEqual({
+            ...FOLDERS_INITIAL_STATE,
+            isDeletingFolder: true
+        });
+    });
+
+    it("should reduce FOLDERS_UPDATE_BEGIN", () => {
+        const action: Types.IFolderAction = {
+            type: ACT_TYPES.FOLDERS_UPDATE_BEGIN
+        };
+        const reduction = foldersReducer(undefined, action);
+        expect(reduction).toEqual({
+            ...FOLDERS_INITIAL_STATE,
+            isSavingFolder: true
+        });
+    });
+});
