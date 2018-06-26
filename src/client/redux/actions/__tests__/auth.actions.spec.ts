697ab91f9a323389ba00a56cc134faea61425220
Destin Moulton <destin.moulton@gmail.com>
2018-06-26 15:18:19 -0600

    Added validation, login, and logout redux tests.

--- src/client/redux/actions/__tests__/auth.actions.spec.ts
+++ src/client/redux/actions/__tests__/auth.actions.spec.ts
@@ -16,10 +16,24 @@ describe("auth.actions", () => {
         fetchMock.restore();
     });
 
-    it("", () => {
+    it("authValidateToken() fetches and handles an invalid response", () => {
+        fetchMock.getOnce("/api/auth/validatetoken", {
+            body: {},
+            status: 400
+        });
+
+        const expectedActions = [{ type: ACT_TYPES.AUTH_LOGOUT }];
+
+        const store = mockStore({ authStore: AUTH_INITIAL_STATE });
+        return store.dispatch(AuthActions.authValidateToken()).then(() => {
+            expect(store.getActions()).toEqual(expectedActions);
+        });
+    });
+
+    it("authValidateToken() fetches and handles a valid token", () => {
         fetchMock.getOnce("/api/auth/validatetoken", {
             body: { status: "valid" },
-            method: "GET"
+            status: 200
         });
 
         const expectedActions = [{ type: ACT_TYPES.AUTH_USER_IS_AUTHENTIC }];
@@ -28,5 +42,72 @@ describe("auth.actions", () => {
         return store.dispatch(AuthActions.authValidateToken()).then(() => {
             expect(store.getActions()).toEqual(expectedActions);
         });
+    });
+
+    it("authValidateToken() fetches and handles an invalid token", () => {
+        fetchMock.getOnce("/api/auth/validatetoken", {
+            body: { status: "invalid" },
+            status: 200
+        });
+
+        const expectedActions = [{ type: ACT_TYPES.AUTH_LOGOUT }];
+
+        const store = mockStore({ authStore: AUTH_INITIAL_STATE });
+        return store.dispatch(AuthActions.authValidateToken()).then(() => {
+            expect(store.getActions()).toEqual(expectedActions);
+        });
+    });
+
+    it("authLoginUser() errors when invalid credentials are passed", () => {
+        fetchMock.postOnce("/api/auth/login", {
+            body: { status: "error" },
+            status: 200
+        });
+
+        const expectedActions = [
+            {
+                type: ACT_TYPES.AUTH_ERROR,
+                message: "Unable to find that username or password."
+            }
+        ];
+
+        const store = mockStore({ authStore: AUTH_INITIAL_STATE });
+        return store
+            .dispatch(AuthActions.authLoginUser("TESTUSER", "TESTPASS"))
+            .then(() => {
+                expect(store.getActions()).toEqual(expectedActions);
+            });
+    });
+
+    it("authLoginUser() authenticates when the credentials are correct", () => {
+        fetchMock.postOnce("/api/auth/login", {
+            body: { status: "success" },
+            status: 200
+        });
+
+        const expectedActions = [
+            {
+                type: ACT_TYPES.AUTH_USER_IS_AUTHENTIC
+            }
+        ];
+
+        const store = mockStore({ authStore: AUTH_INITIAL_STATE });
+        return store
+            .dispatch(AuthActions.authLoginUser("TESTUSER", "TESTPASS"))
+            .then(() => {
+                expect(store.getActions()).toEqual(expectedActions);
+            });
+    });
+
+    it("authLogoutUser() logs out the user", () => {
+        const expectedActions = [
+            {
+                type: ACT_TYPES.AUTH_LOGOUT
+            }
+        ];
+
+        const store = mockStore({ authStore: AUTH_INITIAL_STATE });
+        store.dispatch(AuthActions.authLogoutUser());
+        expect(store.getActions()).toEqual(expectedActions);
     });
 });
