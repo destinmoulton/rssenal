e75c922a25bea77c248879f74351cc91eae0c4d1
Destin Moulton <destin.moulton@gmail.com>
2018-06-27 16:43:54 -0600

    Added test for entriesGetAllForFeed.

--- src/client/redux/actions/__tests__/entries.actions.spec.ts
+++ src/client/redux/actions/__tests__/entries.actions.spec.ts
@@ -4,8 +4,12 @@ import thunk from "redux-thunk";
 
 import * as EntriesActions from "../entries.actions";
 import * as ACT_TYPES from "../../actiontypes";
-import { ENTRIES_INITIAL_STATE } from "../../initialstate";
-import * as Types from "../../../types";
+import * as INIT_STATE from "../../initialstate";
+
+import API_ENTRIES_STRING from "../../../../../test/data/api/entries.unread";
+import AMMENDED_ENTRIES from "../../../../../test/data/immutable/ammendedEntries";
+import IMM_FEEDS from "../../../../../test/data/immutable/feeds";
+import IMM_UNREAD_MAP from "../../../../../test/data/immutable/unreadMap";
 
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
@@ -18,20 +22,34 @@ describe("entries.actions", () => {
     });
 
     it("entriesGetAllForFeed() fetches and ammends entries", () => {
-        fetchMock.getOnce("/api/entries/", {
-            body: {
-                
-            }
-            status: 200
-        });
+        const feedID = "5b33c76cb2438d5708dc197e";
+        const url = "/api/entries/?showEntriesHasRead=false&feedId=" + feedID;
+
+        fetchMock.getOnce(url, JSON.parse(API_ENTRIES_STRING));
 
         const expectedActions = [
-            { type: ACT_TYPES.ENTRIES_SET_ALL, entries: {} }
+            { type: ACT_TYPES.ENTRIES_SET_ALL, entries: AMMENDED_ENTRIES },
+            {
+                type: ACT_TYPES.FILTER_CHANGE,
+                filterTitle: "All",
+                newFilter: { id: "all", limit: "folder" },
+                filteredEntries: AMMENDED_ENTRIES
+            },
+            { type: ACT_TYPES.FEEDS_SET_UNREAD, unreadMap: IMM_UNREAD_MAP }
         ];
 
-        const store = mockStore({ authStore: ENTRIES_INITIAL_STATE });
+        const store = mockStore({
+            entriesStore: INIT_STATE.ENTRIES_INITIAL_STATE,
+            feedsStore: {
+                ...INIT_STATE.FEEDS_INITIAL_STATE,
+                feeds: IMM_FEEDS
+            },
+            filterStore: INIT_STATE.FILTER_INITIAL_STATE,
+            settingsStore: INIT_STATE.SETTINGS_INITIAL_STATE
+        });
+
         return store
-            .dispatch(EntriesActions.entriesGetAllForFeed("FEED_ID"))
+            .dispatch(EntriesActions.entriesGetAllForFeed(feedID))
             .then(() => {
                 expect(store.getActions()).toEqual(expectedActions);
             });
