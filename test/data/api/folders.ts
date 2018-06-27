ad00511e2eecb3085deaba9cca833c1eba602f5f
Destin Moulton <destin.moulton@gmail.com>
2018-06-27 11:44:00 -0600

    Added new folders data.

---
+++ test/data/api/folders.ts
@@ -0,0 +1,16 @@
+export default `
+[
+    {
+        _id: "5b33c4bab2438d5708dc1967",
+        name: "Random",
+        order: 0,
+        __v: 0
+    },
+    {
+        _id: "5b33c57eb2438d5708dc1968",
+        name: "Programming",
+        order: 1,
+        __v: 0
+    }
+]
+`;
