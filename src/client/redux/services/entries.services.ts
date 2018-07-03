1734e1d005753ce2f68375b7dd673a959ee4be4c
Destin Moulton <destin.moulton@gmail.com>
2018-07-03 17:35:46 -0600

    Created an entries services file for the non-action related functions.

---
+++ src/client/redux/services/entries.services.ts
@@ -0,0 +1,95 @@
+import * as moment from "moment";
+
+import { API_ENTRIES_BASE } from "../apiendpoints";
+import { generateJWTJSONHeaders, generateJWTHeaders } from "../../lib/headers";
+import * as Types from "../../types";
+
+export async function getEntriesForFeed(
+    feedId: Types.TFeedID,
+    shouldShowRead: boolean
+) {
+    const queryString = `?showEntriesHasRead=${shouldShowRead}&feedId=${feedId}`;
+    const url = API_ENTRIES_BASE + queryString;
+    const init = {
+        method: "GET",
+        headers: generateJWTHeaders()
+    };
+
+    return fetch(url, init)
+        .then(res => {
+            return res.json();
+        })
+        .then(resObj => {
+            if (resObj.status === "error") {
+                throw new Error(resObj.error);
+            } else if (Array.isArray(resObj.entries)) {
+                return resObj.entries;
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+export function ammendRawEntries(
+    currentFeed: Types.IFeed,
+    rawEntries: Types.IEntry[]
+): Types.IEntry[] {
+    return rawEntries.map((entry: Types.IEntry) => {
+        const feedTitle = currentFeed.title;
+        const timeAgo = moment(entry.publish_date).fromNow();
+        return {
+            ...entry,
+            feedTitle,
+            timeAgo
+        };
+    });
+}
+
+export function addAmmendedEntries(
+    currentEntries: Types.TEntries,
+    ammendedEntries: Types.IEntry[]
+) {
+    let fullEntries = currentEntries.toOrderedMap();
+    ammendedEntries.forEach(entry => {
+        fullEntries = fullEntries.set(entry._id, entry);
+    });
+    return fullEntries;
+}
+
+export async function updateEntryHasRead(
+    entry: Types.IEntry,
+    hasRead: boolean
+) {
+    const url = API_ENTRIES_BASE + entry._id;
+    const init = {
+        method: "PUT",
+        body: JSON.stringify({ has_read: hasRead }),
+        headers: generateJWTJSONHeaders()
+    };
+
+    return fetch(url, init)
+        .then(res => {
+            return res.json();
+        })
+        .then(resObj => {
+            if (resObj.status === "error") {
+                throw new Error(resObj.error);
+            } else {
+                return true;
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+export function ammendEntryReadStatus(
+    currentEntries: Types.TEntries,
+    entryId: Types.TEntryID,
+    hasRead: boolean
+): Types.TEntries {
+    const entry = currentEntries.get(entryId);
+    const newEntry = { ...entry, has_read: hasRead };
+    return currentEntries.set(entryId, newEntry);
+}
