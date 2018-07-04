558717f48d42b226404503d694d0fa2678933898
Destin Moulton <destin.moulton@gmail.com>
2018-07-04 11:34:21 -0600

    Created an Auth Services file to hold the api functions.

---
+++ src/client/redux/services/auth.services.ts
@@ -0,0 +1,58 @@
+import { API_AUTH_LOGIN, API_AUTH_VALIDATE_TOKEN } from "../apiendpoints";
+
+import {
+    generateJWTJSONHeaders,
+    generateJSONHeaders,
+    generateJWTHeaders
+} from "../../lib/headers";
+import * as Types from "../../types";
+
+export async function apiValidateToken() {
+    const url = API_AUTH_VALIDATE_TOKEN;
+    const init = {
+        method: "GET",
+        headers: generateJWTHeaders()
+    };
+    return fetch(url, init)
+        .then(res => {
+            if (!res.ok) {
+                return { status: "invalid" };
+            }
+
+            return res.json();
+        })
+        .then(auth => {
+            if (auth.status === "valid") {
+                return true;
+            } else {
+                throw new Error("User not logged in.");
+            }
+        })
+        .catch(err => {
+            throw new Error(err);
+        });
+}
+
+export async function apiLoginUser(username: string, password: string) {
+    const url = API_AUTH_LOGIN;
+    const init = {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: generateJSONHeaders()
+    };
+
+    return fetch(url, init)
+        .then(res => {
+            return res.json();
+        })
+        .then(auth => {
+            if (auth.status === "success") {
+                return auth.token;
+            } else {
+                throw new Error("Invalid username or password.");
+            }
+        })
+        .catch(err => {
+            throw new Error(err);
+        });
+}
