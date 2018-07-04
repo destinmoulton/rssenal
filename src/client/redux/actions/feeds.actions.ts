209576ba100803f862f09b14d36ed103a5df127c
Destin Moulton <destin.moulton@gmail.com>
2018-07-04 10:54:57 -0600

    Migrated a bunch of api calls into services.

--- src/client/redux/actions/feeds.actions.ts
+++ src/client/redux/actions/feeds.actions.ts
@@ -1,13 +1,13 @@
-import { OrderedMap } from "immutable";
-
 import * as ACT_TYPES from "../actiontypes";
-import { API_FEEDS_BASE } from "../apiendpoints";
+
 import { entriesClearAll, entriesGetAllForFeed } from "./entries.actions";
 import { filterReset } from "./filter.actions";
 import { generateJWTJSONHeaders, generateJWTHeaders } from "../../lib/headers";
 import { message } from "./messages.actions";
-import { propertyComparator } from "../../lib/sort";
+import * as FeedsServices from "../services/feeds.services";
+
 import * as Types from "../../types";
+import { Feed } from "semantic-ui-react";
 
 export function feedInitiateAdd(feedInfo: Types.IFeed) {
     return (dispatch: Types.IDispatch) => {
@@ -23,6 +23,7 @@ function beginAddFeedProcess() {
 }
 
 function addFeed(feedInfo: Types.IFeed) {
+    console.log("addFeed called");
     return (dispatch: Types.IDispatch) => {
         const url = API_FEEDS_BASE;
         const headers = generateJWTJSONHeaders();
@@ -32,7 +33,7 @@ function addFeed(feedInfo: Types.IFeed) {
             headers
         };
 
-        fetch(url, init)
+        return fetch(url, init)
             .then(res => {
                 return res.json();
             })
@@ -69,48 +70,18 @@ function addFeedComplete(feeds: Types.TFeeds) {
 }
 
 export function feedsGetAll() {
-    return (dispatch: Types.IDispatch) => {
-        const url = API_FEEDS_BASE;
-        const init = {
-            method: "GET",
-            headers: generateJWTHeaders()
-        };
-        return fetch(url, init)
-            .then(res => {
-                return res.json();
-            })
-            .then(resObj => {
-                if (resObj.status === "error") {
-                    dispatch(message(resObj.error, "error"));
-                    console.error(resObj.error);
-                } else if (resObj.status === "success") {
-                    dispatch(convertAllFeedsToOrderedMap(resObj.feeds));
-                    dispatch(getAllEntriesForFeeds(resObj.feeds));
-                }
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    };
-}
+    return async (dispatch: Types.IDispatch) => {
+        try {
+            const feedsArr = await FeedsServices.getAllFeeds();
+            let newFeeds = FeedsServices.convertRawFeedsToOrderedMap(feedsArr);
+            newFeeds = FeedsServices.sortFeeds(newFeeds);
 
-function convertAllFeedsToOrderedMap(feedsArr: Types.IFeed[]) {
-    return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
-        const feedsTuples = feedsArr.map(feed => {
-            return [feed._id, feed];
-        });
-        let feedsOrderedMap: Types.TFeeds = OrderedMap(feedsTuples);
-        feedsOrderedMap = sortFeeds(feedsOrderedMap);
-        dispatch(getAllFeedsComplete(feedsOrderedMap));
+            dispatch(getAllFeedsComplete(newFeeds));
+            dispatch(getAllEntriesForFeeds(newFeeds));
+        } catch (err) {
+            dispatch(message(err, "error"));
+        }
     };
-}
-
-function sortFeeds(feeds: Types.TFeeds): Types.TFeeds {
-    return feeds
-        .sort((a: Types.IFeed, b: Types.IFeed) =>
-            propertyComparator(a, b, "asc", "title", true)
-        )
-        .toOrderedMap();
 }
 
 function getAllFeedsComplete(feeds: Types.TFeeds) {
@@ -120,7 +91,7 @@ function getAllFeedsComplete(feeds: Types.TFeeds) {
     };
 }
 
-function getAllEntriesForFeeds(feeds: Types.IFeed[]) {
+function getAllEntriesForFeeds(feeds: Types.TFeeds) {
     return (dispatch: Types.IDispatch) => {
         dispatch(clearUnread());
         feeds.forEach(feed => {
@@ -148,8 +119,18 @@ export function feedsRefreshAll() {
 
 export function beginDeleteFeed(feedId: Types.TFeedID) {
     return (dispatch: Types.IDispatch) => {
-        dispatch(beginDeleteProcess());
         dispatch(deleteFeed(feedId));
+    };
+}
+
+export function deleteFeed(feedId: Types.TFeedID) {
+    return async (dispatch: Types.IDispatch) => {
+        dispatch(beginDeleteProcess());
+
+        await FeedsServices.apiDeleteFeed(feedId);
+        dispatch(message("Feed removed.", "success"));
+        dispatch(filterReset());
+        dispatch(feedsRefreshAll());
     };
 }
 
@@ -159,87 +140,27 @@ function beginDeleteProcess() {
     };
 }
 
-function deleteFeed(feedId: Types.TFeedID) {
-    return (dispatch: Types.IDispatch) => {
-        const url = API_FEEDS_BASE + feedId;
-        const init = {
-            method: "DELETE",
-            headers: generateJWTHeaders()
-        };
-        fetch(url, init)
-            .then(res => {
-                return res.json();
-            })
-            .then(resObj => {
-                dispatch(message("Feed removed.", "success"));
-                dispatch(filterReset());
-                dispatch(deleteFeedComplete(feedId));
-                dispatch(feedsRefreshAll());
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    };
-}
+export function saveFeed(feedInfo: Types.IFeed) {
+    return async (dispatch: Types.IDispatch, getState: Types.IGetState) => {
+        const { feeds } = getState().feedsStore;
 
-function deleteFeedComplete(feedId: Types.TFeedID) {
-    return {
-        type: ACT_TYPES.FEEDS_DELETE_COMPLETE,
-        feedId
-    };
-}
-
-export function feedInitiateUpdate(feedInfo: Types.IFeed) {
-    return (dispatch: Types.IDispatch) => {
         dispatch(beginUpdateFeedProcess());
-        dispatch(updateFeed(feedInfo));
+
+        const updatedFeed = await FeedsServices.apiUpdateFeed(feedInfo);
+
+        dispatch(message("Feed saved.", "success"));
+
+        let newFeeds = FeedsServices.setSingleFeed(updatedFeed, feeds);
+        newFeeds = FeedsServices.sortFeeds(newFeeds);
+        dispatch(updateFeedComplete(newFeeds));
+
+        dispatch(feedsGetAll());
     };
 }
 
 function beginUpdateFeedProcess() {
     return {
         type: ACT_TYPES.FEEDS_UPDATE_BEGIN
-    };
-}
-
-function updateFeed(feedInfo: Types.IFeed) {
-    return (dispatch: Types.IDispatch) => {
-        const url = API_FEEDS_BASE + feedInfo._id;
-        const init = {
-            method: "PUT",
-            body: JSON.stringify({
-                title: feedInfo.title,
-                folder_id: feedInfo.folder_id
-            }),
-            headers: generateJWTJSONHeaders()
-        };
-
-        fetch(url, init)
-            .then(res => {
-                return res.json();
-            })
-            .then(feedObj => {
-                if (feedObj.status === "error") {
-                    console.error(feedObj.error);
-                } else if (feedObj.status === "success") {
-                    dispatch(message("Feed saved.", "success"));
-                    dispatch(applyUpdateFeed(feedObj.feedInfo));
-                    dispatch(feedsGetAll());
-                }
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    };
-}
-
-function applyUpdateFeed(feed: Types.IFeed) {
-    return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
-        const { feeds } = getState().feedsStore;
-
-        let newFeeds = feeds.set(feed._id, feed);
-        newFeeds = sortFeeds(newFeeds);
-        dispatch(updateFeedComplete(newFeeds));
     };
 }
 
@@ -250,45 +171,15 @@ function updateFeedComplete(feeds: Types.TFeeds) {
     };
 }
 
-export function feedsUpdateUnreadCount(entries: Types.IEntry[]) {
+export function feedsUpdateUnreadCount(entries: Types.TEntries) {
     return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
         const { feeds, unreadMap } = getState().feedsStore;
-        let entriesCounted = unreadMap.entriesCounted;
-        let unreadFeeds = unreadMap.feeds;
-        let unreadFolders = unreadMap.folders;
 
-        entries.map((entry: Types.IEntry) => {
-            if (!entriesCounted.has(entry._id) && !entry.has_read) {
-                entriesCounted = entriesCounted.add(entry._id);
-
-                const feed = feeds.get(entry.feed_id);
-
-                if (!unreadFeeds.has(feed._id)) {
-                    unreadFeeds = unreadFeeds.set(feed._id, 1);
-                } else {
-                    const countUnread: number = unreadFeeds.get(feed._id);
-                    unreadFeeds = unreadFeeds.set(feed._id, countUnread + 1);
-                }
-
-                if (!unreadFolders.has(feed.folder_id)) {
-                    unreadFolders = unreadFolders.set(feed.folder_id, 1);
-                } else {
-                    const countUnread: number = unreadFolders.get(
-                        feed.folder_id
-                    );
-                    unreadFolders = unreadFolders.set(
-                        feed.folder_id,
-                        countUnread + 1
-                    );
-                }
-            }
-        });
-
-        const newUnreadMap = {
-            entriesCounted,
-            feeds: unreadFeeds,
-            folders: unreadFolders
-        };
+        const newUnreadMap = FeedsServices.updateUnreadCount(
+            unreadMap,
+            entries,
+            feeds
+        );
 
         dispatch(feedsSetUnread(newUnreadMap));
     };
@@ -297,28 +188,13 @@ export function feedsUpdateUnreadCount(entries: Types.IEntry[]) {
 export function feedsDecrementUnread(feedId: Types.TFeedID) {
     return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
         const { feeds, unreadMap } = getState().feedsStore;
-        const feed = feeds.get(feedId);
 
-        let unreadFeeds = unreadMap.feeds;
-        let unreadFolders = unreadMap.folders;
+        const newUnreadMap = FeedsServices.decrementUnread(
+            feedId,
+            feeds,
+            unreadMap
+        );
 
-        if (unreadFeeds.has(feedId)) {
-            const unreadFeedCount = unreadFeeds.get(feedId);
-            const newCount = unreadFeedCount > 1 ? unreadFeedCount - 1 : 0;
-            unreadFeeds = unreadFeeds.set(feedId, newCount);
-        }
-
-        if (unreadFolders.has(feed.folder_id)) {
-            const unreadFolderCount = unreadFolders.get(feed.folder_id);
-            const newCount = unreadFolderCount > 1 ? unreadFolderCount - 1 : 0;
-            unreadFolders = unreadFolders.set(feed.folder_id, newCount);
-        }
-
-        const newUnreadMap = {
-            ...unreadMap,
-            feeds: unreadFeeds,
-            folders: unreadFolders
-        };
         dispatch(feedsSetUnread(newUnreadMap));
     };
 }
--- src/client/redux/services/feeds.services.ts
+++ src/client/redux/services/feeds.services.ts
@@ -1,49 +1,162 @@
+import { OrderedMap } from "immutable";
+
+import { API_FEEDS_BASE } from "../apiendpoints";
 import { generateJWTJSONHeaders, generateJWTHeaders } from "../../lib/headers";
+import { propertyComparator } from "../../lib/sort";
 import * as Types from "../../types";
 
-export function feedsUpdateUnreadCount(
-    unreadMap: Types.IFeedsUnreadMap,
-    entries: Types.IEntry[],
-    feeds: Types.TFeeds
-) {
-    return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
-        let entriesCounted = unreadMap.entriesCounted;
-        let unreadFeeds = unreadMap.feeds;
-        let unreadFolders = unreadMap.folders;
+export function convertRawFeedsToOrderedMap(
+    feedsArr: Types.IFeed[]
+): Types.TFeeds {
+    const feedsTuples = feedsArr.map(feed => {
+        return [feed._id, feed];
+    });
+    return OrderedMap(feedsTuples);
+}
 
-        entries.map((entry: Types.IEntry) => {
-            if (!entriesCounted.has(entry._id) && !entry.has_read) {
-                entriesCounted = entriesCounted.add(entry._id);
-
-                const feed = feeds.get(entry.feed_id);
-
-                if (!unreadFeeds.has(feed._id)) {
-                    unreadFeeds = unreadFeeds.set(feed._id, 1);
-                } else {
-                    const countUnread: number = unreadFeeds.get(feed._id);
-                    unreadFeeds = unreadFeeds.set(feed._id, countUnread + 1);
-                }
-
-                if (!unreadFolders.has(feed.folder_id)) {
-                    unreadFolders = unreadFolders.set(feed.folder_id, 1);
-                } else {
-                    const countUnread: number = unreadFolders.get(
-                        feed.folder_id
-                    );
-                    unreadFolders = unreadFolders.set(
-                        feed.folder_id,
-                        countUnread + 1
-                    );
-                }
+export async function getAllFeeds() {
+    const url = API_FEEDS_BASE;
+    const init = {
+        method: "GET",
+        headers: generateJWTHeaders()
+    };
+    return fetch(url, init)
+        .then(res => {
+            return res.json();
+        })
+        .then(resObj => {
+            if (resObj.status === "error") {
+                throw new Error(resObj.error);
+            } else if (resObj.status === "success") {
+                return true;
             }
         });
+}
 
-        const newUnreadMap = {
-            entriesCounted,
-            feeds: unreadFeeds,
-            folders: unreadFolders
-        };
+export function updateUnreadCount(
+    unreadMap: Types.IFeedsUnreadMap,
+    entries: Types.TEntries,
+    feeds: Types.TFeeds
+) {
+    let entriesCounted = unreadMap.entriesCounted;
+    let unreadFeeds = unreadMap.feeds;
+    let unreadFolders = unreadMap.folders;
 
-        dispatch(feedsSetUnread(newUnreadMap));
+    entries.map((entry: Types.IEntry) => {
+        if (!entriesCounted.has(entry._id) && !entry.has_read) {
+            entriesCounted = entriesCounted.add(entry._id);
+
+            const feed = feeds.get(entry.feed_id);
+
+            if (!unreadFeeds.has(feed._id)) {
+                unreadFeeds = unreadFeeds.set(feed._id, 1);
+            } else {
+                const countUnread: number = unreadFeeds.get(feed._id);
+                unreadFeeds = unreadFeeds.set(feed._id, countUnread + 1);
+            }
+
+            if (!unreadFolders.has(feed.folder_id)) {
+                unreadFolders = unreadFolders.set(feed.folder_id, 1);
+            } else {
+                const countUnread: number = unreadFolders.get(feed.folder_id);
+                unreadFolders = unreadFolders.set(
+                    feed.folder_id,
+                    countUnread + 1
+                );
+            }
+        }
+    });
+
+    return {
+        entriesCounted,
+        feeds: unreadFeeds,
+        folders: unreadFolders
     };
+}
+
+export function decrementUnread(
+    feedId: Types.TFeedID,
+    feeds: Types.TFeeds,
+    unreadMap: Types.IFeedsUnreadMap
+) {
+    const feed = feeds.get(feedId);
+
+    let unreadFeeds = unreadMap.feeds;
+    let unreadFolders = unreadMap.folders;
+
+    if (unreadFeeds.has(feedId)) {
+        const unreadFeedCount = unreadFeeds.get(feedId);
+        const newCount = unreadFeedCount > 1 ? unreadFeedCount - 1 : 0;
+        unreadFeeds = unreadFeeds.set(feedId, newCount);
+    }
+
+    if (unreadFolders.has(feed.folder_id)) {
+        const unreadFolderCount = unreadFolders.get(feed.folder_id);
+        const newCount = unreadFolderCount > 1 ? unreadFolderCount - 1 : 0;
+        unreadFolders = unreadFolders.set(feed.folder_id, newCount);
+    }
+
+    return {
+        ...unreadMap,
+        feeds: unreadFeeds,
+        folders: unreadFolders
+    };
+}
+
+export async function apiUpdateFeed(feedInfo: Types.IFeed) {
+    const url = API_FEEDS_BASE + feedInfo._id;
+    const init = {
+        method: "PUT",
+        body: JSON.stringify({
+            title: feedInfo.title,
+            folder_id: feedInfo.folder_id
+        }),
+        headers: generateJWTJSONHeaders()
+    };
+
+    return fetch(url, init)
+        .then(res => {
+            return res.json();
+        })
+        .then(feedObj => {
+            if (feedObj.status === "error") {
+                throw new Error(feedObj.error);
+            } else if (feedObj.status === "success") {
+                return feedObj.feedInfo;
+            }
+        });
+}
+
+export function setSingleFeed(feed: Types.IFeed, feeds: Types.TFeeds) {
+    return feeds.set(feed._id, feed);
+}
+
+export function sortFeeds(feeds: Types.TFeeds): Types.TFeeds {
+    return feeds
+        .sort((a: Types.IFeed, b: Types.IFeed) =>
+            propertyComparator(a, b, "asc", "title", true)
+        )
+        .toOrderedMap();
+}
+
+export async function apiDeleteFeed(feedID: Types.TFeedID) {
+    const url = API_FEEDS_BASE + feedID;
+    const init = {
+        method: "DELETE",
+        headers: generateJWTHeaders()
+    };
+    return fetch(url, init)
+        .then(res => {
+            return res.json();
+        })
+        .then(resObj => {
+            if (resObj.status === "error") {
+                throw new Error(resObj.error);
+            } else if (resObj.status === "success") {
+                return true;
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
