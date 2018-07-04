554be97806868893419ef3c079d1ec3ff60d677e
Destin Moulton <destin.moulton@gmail.com>
2018-07-04 11:54:49 -0600

    Removed unneeded filter method; added call to service get title.

--- src/client/redux/actions/filter.actions.ts
+++ src/client/redux/actions/filter.actions.ts
@@ -1,59 +1,23 @@
 import { OrderedMap } from "immutable";
 
 import { FILTER_CHANGE, FILTER_RESET } from "../actiontypes";
+import * as FilterServices from "../services/filter.services";
 import * as Types from "../../types";
 
-export function filterChangeActive(filter: Types.IFilter) {
+export function filterVisibleEntries(filter: Types.IFilter) {
     return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
-        const { entriesStore } = getState();
-        dispatch(filterVisibleEntries(filter, entriesStore.entries));
-    };
-}
+        const { entriesStore, feedsStore, foldersStore } = getState();
+        const feeds = feedsStore.feeds;
+        const folders = foldersStore.folders;
+        const entries = entriesStore.entries;
 
-export function filterVisibleEntries(
-    filter: Types.IFilter,
-    currentEntries: Types.TEntries
-) {
-    return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
-        const { feedsStore, foldersStore } = getState();
-        const allFeeds = feedsStore.feeds;
+        const title = FilterServices.getFilterTitle(filter, feeds, folders);
+        const filteredEntries = FilterServices.getFilteredEntries(
+            filter,
+            entries,
+            feeds
+        );
 
-        let title = "All";
-
-        let filteredEntries = currentEntries.toOrderedMap();
-        switch (filter.limit) {
-            case "feed":
-                const activeFeed = feedsStore.feeds.get(filter.id);
-                title = activeFeed.title;
-
-                filteredEntries = currentEntries
-                    .filter(entry => {
-                        return entry.feed_id === filter.id;
-                    })
-                    .toOrderedMap();
-                break;
-            case "folder":
-                if (filter.id !== "all") {
-                    const activeFolder = foldersStore.folders.get(filter.id);
-
-                    title = activeFolder.name;
-
-                    const feedIds = allFeeds
-                        .filter(feed => {
-                            return feed.folder_id === filter.id;
-                        })
-                        .map(feed => {
-                            return feed._id;
-                        });
-
-                    filteredEntries = currentEntries
-                        .filter(entry => {
-                            return feedIds.includes(entry.feed_id);
-                        })
-                        .toOrderedMap();
-                }
-                break;
-        }
         dispatch(setNewFilter(filter, title, filteredEntries));
     };
 }
