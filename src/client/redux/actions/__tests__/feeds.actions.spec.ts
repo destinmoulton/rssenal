f82f27f70cdea5e9993cc15d9b733bdf88970985
Destin Moulton <destin.moulton@gmail.com>
2018-07-06 16:51:44 -0600

    Added feed actions spec/test.

--- src/client/redux/actions/__tests__/feeds.actions.spec.ts
+++ src/client/redux/actions/__tests__/feeds.actions.spec.ts
@@ -1,19 +1,15 @@
-import { List, OrderedMap } from "immutable";
 import * as fetchMock from "fetch-mock";
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 
 import * as FeedsActions from "../feeds.actions";
-import * as ACT_TYPES from "../../actiontypes";
 import * as INIT_STATE from "../../initialstate";
 
 import API_ENTRIES_STRING from "../../../../../test/data/api/entries.unread";
 import API_FEEDS_STRING from "../../../../../test/data/api/feeds";
 import FEED from "../../../../../test/data/feed";
-import AMMENDED_ENTRIES from "../../../../../test/data/immutable/ammendedEntries";
+import IMM_ENTRIES from "../../../../../test/data/immutable/ammendedEntries";
 import IMM_FEEDS from "../../../../../test/data/immutable/feeds";
-import * as IMM_UNREAD from "../../../../../test/data/immutable/unreadMap";
-import feed from "../../../../../test/data/feed";
 
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
@@ -114,7 +110,17 @@ describe("feeds.actions", () => {
     it("deleteFeed() handles refreshing entries for current feeds", () => {
         const feedID = "5b33c76cb2438d5708dc197e";
         const url = "/api/feeds/" + feedID;
-        fetchMock.deleteOnce(url, { status: "success" });
+        fetchMock.deleteOnce(url, { body: { status: "success" } });
+
+        const feed_url = "/api/feeds/";
+
+        fetchMock.getOnce(feed_url, JSON.parse(API_FEEDS_STRING));
+
+        const entries_url =
+            "/api/entries/?showEntriesHasRead=false&feedId=" + feedID;
+
+        fetchMock.getOnce(entries_url, JSON.parse(API_ENTRIES_STRING));
+
         const store = mockStore({
             entriesStore: INIT_STATE.ENTRIES_INITIAL_STATE,
             feedsStore: {
@@ -127,7 +133,69 @@ describe("feeds.actions", () => {
             settingsStore: INIT_STATE.SETTINGS_INITIAL_STATE
         });
 
-        store.dispatch(FeedsActions.deleteFeed(feedID));
+        return store.dispatch(FeedsActions.deleteFeed(feedID)).then(() => {
+            expect(store.getActions()).toMatchSnapshot();
+        });
+    });
+
+    it("saveFeed() handles refreshing entries for current feeds", () => {
+        const feedID = "5b33c76cb2438d5708dc197e";
+        const url = "/api/feeds/" + feedID;
+        fetchMock.putOnce(url, { status: "success", feedInfo: FEED });
+
+        const feed_url = "/api/feeds/";
+
+        fetchMock.getOnce(feed_url, JSON.parse(API_FEEDS_STRING));
+
+        const entries_url =
+            "/api/entries/?showEntriesHasRead=false&feedId=" + feedID;
+
+        fetchMock.getOnce(entries_url, JSON.parse(API_ENTRIES_STRING));
+
+        const store = mockStore({
+            entriesStore: INIT_STATE.ENTRIES_INITIAL_STATE,
+            feedsStore: {
+                ...INIT_STATE.FEEDS_INITIAL_STATE,
+                feeds: IMM_FEEDS
+            },
+            filterStore: INIT_STATE.FILTER_INITIAL_STATE,
+            foldersStore: INIT_STATE.FOLDERS_INITIAL_STATE,
+            messagesStore: INIT_STATE.MESSAGES_INITIAL_STATE,
+            settingsStore: INIT_STATE.SETTINGS_INITIAL_STATE
+        });
+
+        const updatedFeed = {
+            _id: feedID,
+            title: "FEED_TITLE",
+            folder_id: "FOLDER_ID",
+            description: "DESCRIPTION",
+            link: "LINK_HERE"
+        };
+        return store.dispatch(FeedsActions.saveFeed(updatedFeed)).then(() => {
+            expect(store.getActions()).toMatchSnapshot();
+        });
+    });
+
+    it("feedsUpdateUnreadCount() handles updating the unread count", () => {
+        const store = mockStore({
+            feedsStore: {
+                ...INIT_STATE.FEEDS_INITIAL_STATE,
+                feeds: IMM_FEEDS
+            }
+        });
+        store.dispatch(FeedsActions.feedsUpdateUnreadCount(IMM_ENTRIES));
+        expect(store.getActions()).toMatchSnapshot();
+    });
+
+    it("feedsDecrementUnread() handles decrementing the read count", () => {
+        const feedID = "5b33c76cb2438d5708dc197e";
+        const store = mockStore({
+            feedsStore: {
+                ...INIT_STATE.FEEDS_INITIAL_STATE,
+                feeds: IMM_FEEDS
+            }
+        });
+        store.dispatch(FeedsActions.feedsDecrementUnread(feedID));
         expect(store.getActions()).toMatchSnapshot();
     });
 });
