d674ae5f02ef809f6d07a8317cd2beed4a3300dd
Destin Moulton <destin.moulton@gmail.com>
2018-07-14 16:46:00 -0600

    Added spec and snapshot for RightPaneContainer.

---
+++ src/client/containers/RightPane/__tests__/RightPaneContainer.spec.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import * as fetchMock from "fetch-mock";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import { mount } from "enzyme";
+
+import RightPaneContainer from "../RightPaneContainer";
+import * as INIT_STATE from "../../../redux/initialstate";
+import AMMENDED_ENTRIES from "../../../../../test/data/immutable/ammendedEntries";
+import IMM_FEEDS from "../../../../../test/data/immutable/feeds";
+import IMM_FOLDERS from "../../../../../test/data/immutable/folders";
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe("<RightPaneContainer />", () => {
+    afterEach(() => {
+        fetchMock.reset();
+        fetchMock.restore();
+        localStorage.clear();
+    });
+
+    it("renders and matches the snapshot", async () => {
+        const store = mockStore({
+            entriesStore: {
+                ...INIT_STATE.ENTRIES_INITIAL_STATE,
+                entries: AMMENDED_ENTRIES
+            },
+            feedsStore: {
+                ...INIT_STATE.FEEDS_INITIAL_STATE,
+                feeds: IMM_FEEDS
+            },
+            filterStore: {
+                ...INIT_STATE.FILTER_INITIAL_STATE,
+                filteredEntries: AMMENDED_ENTRIES
+            },
+            foldersStore: {
+                ...INIT_STATE.FOLDERS_INITIAL_STATE,
+                folders: IMM_FOLDERS
+            },
+            settingsStore: INIT_STATE.SETTINGS_INITIAL_STATE
+        });
+
+        const wrapper = mount(
+            <Provider store={store}>
+                <RightPaneContainer />
+            </Provider>
+        );
+
+        try {
+            await fetchMock.flush();
+
+            expect(wrapper).toMatchSnapshot();
+        } catch (err) {
+            throw err;
+        }
+    });
+});
