87044ad34e128307e655a5ab4b3f19bdaa569a6d
Destin Moulton <destin.moulton@gmail.com>
2018-09-18 12:01:08 -0600

    Modified to create log dir.

--- src/server/lib/logger.js
+++ src/server/lib/logger.js
@@ -1,12 +1,24 @@
 const winston = require("winston");
+const { format } = winston;
+
+var fs = require("fs");
+var path = require("path");
+var logDir = path.join(process.env.PWD, "log"); // directory path you want to set
+if (!fs.existsSync(logDir)) {
+    // Create the directory if it does not exist
+    fs.mkdirSync(logDir);
+}
+
 const logger = winston.createLogger({
+    level: "info",
+    format: format.combine(format.splat(), format.simple()),
     transports: [
         new winston.transports.Console({ json: false, timestamp: true }),
         new winston.transports.File({
-            filename: __dirname + "/log/debug.log",
+            filename: path.join(logDir, "info.log"),
             json: false
         })
     ]
 });
 
-exports.default = logger;
+export default logger;
