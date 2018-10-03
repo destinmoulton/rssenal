0c4b65404fe4bafc7115ea3467b0fefaeb1e2d4e
Destin Moulton <destin.moulton@gmail.com>
2018-10-03 13:04:18 -0600

    Added log messages via debug().

--- src/client/components/LeftPane/ButtonBarComponent.tsx
+++ src/client/components/LeftPane/ButtonBarComponent.tsx
@@ -1,9 +1,12 @@
+import debug from "debug";
 import * as React from "react";
 
 import { Button, Icon, Popup } from "semantic-ui-react";
 
 import LogoutButtonContainer from "../../containers/LeftPane/LogoutButtonContainer";
 import ReorderFoldersModalContainer from "../../containers/Modals/ReorderFoldersModalContainer";
+
+const log = debug("ButtonBarComponent");
 
 export interface IButtonBarMapState {}
 export interface IButtonBarMapDispatch {
@@ -19,18 +22,22 @@ type TAll = IButtonBarProps & IButtonBarMapState & IButtonBarMapDispatch;
 
 class ButtonBarComponent extends React.Component<TAll> {
     _handleAddFolder = () => {
+        log("_handleAddFolder()");
         this.props.openEditFolderModal({ _id: "", name: "" });
     };
 
     _handleAddFeed = () => {
+        log("_handleAddFeed()");
         this.props.openAddFeedModal({ _id: "", title: "" });
     };
 
     _handleClickRefresh = () => {
+        log("_handleClickRefresh()");
         this.props.feedsRefreshAll();
     };
 
     render() {
+        log("render()");
         return (
             <div className="rss-leftmenu-buttonbar-container">
                 <Button.Group size="mini">
